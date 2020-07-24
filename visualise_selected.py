1db50bcf1751b647b45d0bfa81c99eaa32414292
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-07-24 13:52:51 +0200

    Readme update

--- visualise_selected.py
+++ visualise_selected.py
@@ -2,4 +2,9 @@ import swiftsimio as sw
 import numpy as np
 
 lines = np.loadtxt("halo_selected.txt", comments="#", delimiter=",", unpack=False).T
-print(lines)
+print("log10(M200c / Msun): ", np.log10(lines[1]*1e13))
+print("R200c: ", lines[2])
+print("Centre of Potential cordinates: (xC, yC, zC)")
+for i in range(3):
+    print(f"\t{lines[3,i]}, {lines[4,i]}, {lines[5,i]}")
+
