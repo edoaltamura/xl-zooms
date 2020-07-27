2f2d566b0a58180645fa604a1fa1f7785be56f18
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-07-27 19:48:45 +0200

    Snap profiles

--- parent/density_profiles.py
+++ parent/density_profiles.py
@@ -25,53 +25,39 @@ x = lines[3]
 y = lines[4]
 z = lines[5]
 
+############################################################
+# DENSITY PROFILE FROM SNAPSHOT - ALL PARTICLES
+dataPath = "/cosma7/data/dp004/jch/EAGLE-XL/DMONLY/Cosma7/L0300N0564/snapshots/"
+snapFile = dataPath + "EAGLE-XL_L0300N0564_DMONLY_0036.hdf5"
 
-snapshot_path = "/cosma7/data/dp004/jch/EAGLE-XL/DMONLY/Cosma7/L0300N0564/snapshots/"
-vrPath = snapshot_path + "stf_swiftdm_3dfof_subhalo_0036/"
-velociraptor_base_name = vrPath + "stf_swiftdm_3dfof_subhalo_0036.VELOCIraptor.properties.0"
-velociraptor_properties = velociraptor_base_name
-velociraptor_groups = velociraptor_base_name.replace("properties", "catalog_groups")
-
-filenames = {
-    "parttypes_filename": velociraptor_base_name.replace(
-        "properties", "catalog_partypes"
-    ),
-    "particles_filename": velociraptor_base_name.replace(
-        "properties", "catalog_particles"
-    ),
-    "unbound_parttypes_filename": velociraptor_base_name.replace(
-        "properties", "catalog_partypes.unbound"
-    ),
-    "unbound_particles_filename": velociraptor_base_name.replace(
-        "properties", "catalog_particles.unbound"
-    ),
-}
-
-catalogue = load(velociraptor_properties)
-groups = load_groups(velociraptor_groups, catalogue)
-
-for halo_id in halo_ids:
-    particles, unbound_particles = groups.extract_halo(halo_id, filenames=filenames)
-
-    M200c = catalogue.masses.mass_200mean[halo_id].to("Solar_Mass")
-    R200c = catalogue.apertures.mass_star_30_kpc[halo_id].to("Solar_Mass")
-
-    # This reads particles using the cell metadata that are around our halo
-    data = to_swiftsimio_dataset(particles, snapshot_path, generate_extra_mask=False)
-    x = particles.x / data.metadata.a
-    y = particles.y / data.metadata.a
-    z = particles.z / data.metadata.a
-    r = np.sqrt(x **2 + y ** 2 + z ** 2)/R200c
+for i in range(3):
+    print(f"Profiling halo {i}...")
+    xCen = unyt.unyt_quantity(x[i], unyt.Mpc)
+    yCen = unyt.unyt_quantity(y[i], unyt.Mpc)
+    zCen = unyt.unyt_quantity(z[i], unyt.Mpc)
+    size = unyt.unyt_quantity(12. * R200c[i], unyt.Mpc)
+    mask = sw.mask(snapFile)
+    region = [
+        [xCen - size, xCen + size],
+        [yCen - size, yCen + size],
+        [zCen - size, zCen + size]
+    ]
+    mask.constrain_spatial(region)
+    data = sw.load(snapFile, mask=mask)
+    posDM = data.dark_matter.coordinates / data.metadata.a
+    r = np.sqrt(posDM[:, 0] ** 2 + posDM[:, 1] ** 2 + posDM[:, 2] ** 2) / R200c
     masses = np.ones_like(r)
 
     # constuct bins for the histogram
-    lbins = np.logspace(-3, 0, 40, base=10.0)
+    lbins = np.logspace(-3, 1, 40, base=10.0)
     # compute statistics - each bin has Y value of the sum of the masses of points within the bin X
     [stat, bin_edge, binnumber] = binned_statistic(r, masses, 'sum', lbins)
     bin_centre = np.sqrt(bin_edge[1:] * bin_edge[:-1])
     # compute radial density distribution
-    densities = stat * masses / ((4. * np.pi * (R200c ** 3) / 3.) * ((bin_edge[1:]) ** 3 - (bin_edge[:-1]) ** 3)) / data.metadata.rho_crit #rho_crit
-
+    volume_shell = (4. * np.pi * (R200c ** 3) / 3.) * ((bin_edge[1:]) ** 3 - (bin_edge[:-1]) ** 3)
+    rho_crit = data.metadata.cosmology['Critical density [internal units]'][0]
+    densities = stat / volume_shell / rho_crit
+    # Plot density profile for each selected halo in volume
     fig, ax = plt.subplots()
     ax.scatter(bin_centre, densities, color="C0", marker=".")
     ax.set_xscale('log')
@@ -79,5 +65,46 @@ for halo_id in halo_ids:
     ax.set_ylabel("Density/rho_crit")
     ax.set_xlabel("Radius/R200c")
     fig.tight_layout()
-    fig.savefig(f"outfiles/halo{i}_density_profile.png")
+    fig.savefig(f"outfiles/halo{i}_density_profile_allparts.png")
     plt.close(fig)
+
+############################################################
+# DENSITY PROFILE FROM SNAPSHOT - SUBHALOS PARTICLES
+
+# snapshot_path = "/cosma7/data/dp004/jch/EAGLE-XL/DMONLY/Cosma7/L0300N0564/snapshots/"
+# vrPath = snapshot_path + "stf_swiftdm_3dfof_subhalo_0036/"
+# velociraptor_base_name = vrPath + "stf_swiftdm_3dfof_subhalo_0036.VELOCIraptor.properties.0"
+# velociraptor_properties = velociraptor_base_name
+# velociraptor_groups = velociraptor_base_name.replace("properties", "catalog_groups")
+#
+# filenames = {
+#     "parttypes_filename": velociraptor_base_name.replace(
+#         "properties", "catalog_partypes"
+#     ),
+#     "particles_filename": velociraptor_base_name.replace(
+#         "properties", "catalog_particles"
+#     ),
+#     "unbound_parttypes_filename": velociraptor_base_name.replace(
+#         "properties", "catalog_partypes.unbound"
+#     ),
+#     "unbound_particles_filename": velociraptor_base_name.replace(
+#         "properties", "catalog_particles.unbound"
+#     ),
+# }
+#
+# catalogue = load(velociraptor_properties)
+# groups = load_groups(velociraptor_groups, catalogue)
+#
+# for halo_id in halo_ids:
+#     particles, unbound_particles = groups.extract_halo(halo_id, filenames=filenames)
+#
+#     M200c = catalogue.masses.mass_200mean[halo_id].to("Solar_Mass")
+#     R200c = catalogue.apertures.mass_star_30_kpc[halo_id].to("Solar_Mass")
+#
+#     # This reads particles using the cell metadata that are around our halo
+#     data = to_swiftsimio_dataset(particles, snapshot_path, generate_extra_mask=False)
+#     x = particles.x / data.metadata.a
+#     y = particles.y / data.metadata.a
+#     z = particles.z / data.metadata.a
+#     r = np.sqrt(x ** 2 + y ** 2 + z ** 2) / R200c
+#     masses = np.ones_like(r)
