6348ca31777f6f61da4d8bb168755ed6929435e4
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-07-27 20:32:07 +0200

    Snap profiles

--- parent/density_profiles.py
+++ parent/density_profiles.py
@@ -52,7 +52,7 @@ for i in range(3):
     masses = np.ones_like(r)
 
     # constuct bins for the histogram
-    lbins = np.logspace(-3, 1, 50)
+    lbins = np.logspace(-2, 1, 40)
     # compute statistics - each bin has Y value of the sum of the masses of points within the bin X
     hist, bin_edges = np.histogram(r, bins=lbins)
     bin_centre = np.sqrt(bin_edges[1:] * bin_edges[:-1])
