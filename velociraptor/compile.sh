08434a0994cb5178c8b84729049da81ecce9d402
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-08-10 19:29:22 +0200

    stf compile.sh

---
+++ velociraptor/compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+# Bash script that configures and compiles VELOCIRAPTOR-STF.
+
+source modules.sh
+
+old_directory=$(pwd)
+
+if [ ! -d ~/VELOCIraptor-STF ]; then
+  echo VELOCIraptor-STF source code not found - cloning from GitLab...
+  cd ~
+  git clone https://github.com/pelahi/VELOCIraptor-STF
+  cd $old_directory
+fi
+
+cd ~/VELOCIraptor-STF
+git fetch
+
+# Configure makefile. Compile into executable ./stf
+cmake . -DVR_ZOOM_SIM=ON -DCMAKE_BUILD_TYPE=Release
+make -j
+
+# Copy executable into xl-zooms directory
+cd $old_directory
+rm ./stf
+cp ~/VELOCIraptor-STF/stf .
