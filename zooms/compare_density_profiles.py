2e5c3075263396cb835a5d42569ecc3f9da28f87
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-08-21 15:10:02 +0100

    Density profiles

--- zooms/compare_density_profiles.py
+++ zooms/compare_density_profiles.py
@@ -13,10 +13,16 @@ sys.path.append("..") # Adds higher directory to python modules path.
 from zooms import density_profiles as zoom
 from parent import density_profiles as parent
 
+author = "SK"
+out_to_radius = 3
+output_directory = "outfiles/"
+
+
 for i in range(3):
     fig, ax = parent.density_profile(i, outfig=True)
     zoom.density_profile(i, outfig=True)
     fig.tight_layout()
-    fig.savefig(f"{output_directory}halo{halo_id}{author}_density_profile_zoom.png")
+    fig.savefig(f"{output_directory}halo{i}{author}_density_profile_compare_zoom.png")
+    plt.close(fig)
 
 
