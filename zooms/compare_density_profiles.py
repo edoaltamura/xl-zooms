16b37fd6f2c9a59eddee1cc32c16fd107b30b135
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-08-24 17:34:10 +0100

    Density profiles

--- zooms/compare_density_profiles.py
+++ zooms/compare_density_profiles.py
@@ -123,7 +123,7 @@ rho_crit = unyt.unyt_quantity(
     data.metadata.cosmology['Critical density [internal units]'],
     unitMass / unitLength ** 3
 )
-particleMasses = data.dark_matter.masses
+particleMasses = data.dark_matter.masses.to('Msun')
 print(particleMasses)
 
 # Construct bins and compute density profile
