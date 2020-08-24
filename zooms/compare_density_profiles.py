c584dd42489a4733a1093a62cf59f919d336cdd4
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-08-24 19:17:21 +0100

    Density profiles

--- zooms/compare_density_profiles.py
+++ zooms/compare_density_profiles.py
@@ -15,6 +15,10 @@ try:
 except:
     pass
 
+# Constants
+bins = 40
+radius_bounds = [1e-2, 3]  # In units of R200crit
+
 
 def latex_float(f):
     float_str = "{0:.2g}".format(f)
@@ -25,156 +29,177 @@ def latex_float(f):
         return float_str
 
 
-# INPUTS
+def density_profile_compare_plot(
+        halo_id: int,
+        author: str,
+        snap_filepath_parent: str = None,
+        snap_filepath_zoom: str = None,
+        velociraptor_properties_zoom: str = None,
+        output_directory: str = None
+) -> None:
+    # PARENT #
+    # Load VR output gathered from the halo selection process
+    lines = np.loadtxt(f"{output_directory}halo_selected_{author}.txt", comments="#", delimiter=",", unpack=False).T
+    M200c = lines[1] * 1e13
+    R200c = lines[2]
+    Xcminpot = lines[3]
+    Ycminpot = lines[4]
+    Zcminpot = lines[5]
+    M200c = unyt.unyt_quantity(M200c[halo_id], unyt.Solar_Mass)
+    R200c = unyt.unyt_quantity(R200c[halo_id], unyt.Mpc)
+    xCen = unyt.unyt_quantity(Xcminpot[halo_id], unyt.Mpc)
+    yCen = unyt.unyt_quantity(Ycminpot[halo_id], unyt.Mpc)
+    zCen = unyt.unyt_quantity(Zcminpot[halo_id], unyt.Mpc)
 
-author = "SK"
-halo_id = 1
-out_to_radius = 5
+    # Construct spatial mask to feed into swiftsimio
+    size = radius_bounds[1] * R200c
+    mask = sw.mask(snap_filepath_parent)
+    region = [
+        [xCen - size, xCen + size],
+        [yCen - size, yCen + size],
+        [zCen - size, zCen + size]
+    ]
+    mask.constrain_spatial(region)
+    data = sw.load(snap_filepath_parent, mask=mask)
 
-snapFile = "/cosma7/data/dp004/jch/EAGLE-XL/DMONLY/Cosma7/L0300N0564/snapshots/EAGLE-XL_L0300N0564_DMONLY_0036.hdf5"
-output_directory = "outfiles/"
+    # Get DM particle coordinates and compute radial distance from CoP in R200 units
+    posDM = data.dark_matter.coordinates / data.metadata.a
+    r = np.sqrt(
+        (posDM[:, 0] - xCen) ** 2 +
+        (posDM[:, 1] - yCen) ** 2 +
+        (posDM[:, 2] - zCen) ** 2
+    ) / R200c
 
-##########################################################
-# PARENT
+    # Calculate particle mass and rho_crit
+    unitLength = data.metadata.units.length
+    unitMass = data.metadata.units.mass
+    rho_crit = unyt.unyt_quantity(
+        data.metadata.cosmology['Critical density [internal units]'],
+        unitMass / unitLength ** 3
+    )
+    rhoMean = rho_crit * data.metadata.cosmology['Omega_m']
+    vol = data.metadata.boxsize[0] ** 3
+    numPart = data.metadata.n_dark_matter
+    particleMass = rhoMean * vol / numPart
+    parent_mass_resolution = particleMass
 
-lines = np.loadtxt(f"{output_directory}halo_selected_{author}.txt", comments="#", delimiter=",", unpack=False).T
-M200c = lines[1] * 1e13
-R200c = lines[2]
-Xcminpot = lines[3]
-Ycminpot = lines[4]
-Zcminpot = lines[5]
-M200c = unyt.unyt_quantity(M200c[halo_id], unyt.Solar_Mass )
-R200c = unyt.unyt_quantity(R200c[halo_id], unyt.Mpc)
-xCen = unyt.unyt_quantity(Xcminpot[halo_id], unyt.Mpc)
-yCen = unyt.unyt_quantity(Ycminpot[halo_id], unyt.Mpc)
-zCen = unyt.unyt_quantity(Zcminpot[halo_id], unyt.Mpc)
+    # Construct bins and compute density profile
+    lbins = np.logspace(np.log10(radius_bounds[0]), np.log10(radius_bounds[1]), bins)
+    hist, bin_edges = np.histogram(r, bins=lbins)
+    bin_centre = np.sqrt(bin_edges[1:] * bin_edges[:-1])
+    volume_shell = (4. * np.pi / 3.) * (R200c ** 3) * ((bin_edges[1:]) ** 3 - (bin_edges[:-1]) ** 3)
+    densities = hist * particleMass / volume_shell / rho_crit
 
-# Construct spatial mask to feed into swiftsimio
-size = out_to_radius * R200c
-mask = sw.mask(snapFile)
-region = [
-    [xCen - size, xCen + size],
-    [yCen - size, yCen + size],
-    [zCen - size, zCen + size]
-]
-mask.constrain_spatial(region)
-data = sw.load(snapFile, mask=mask)
+    # Plot density profile for each selected halo in volume
+    fig, ax = plt.subplots()
+    parent_label = f'Parent: $m_\\mathrm{{DM}} = {latex_float(parent_mass_resolution.value[0])}\\ {parent_mass_resolution.units.latex_repr}$'
+    ax.plot(bin_centre, densities, c="lime", linestyle="-", label=parent_label)
 
-# Get DM particle coordinates and compute radial distance from CoP in R200 units
-posDM = data.dark_matter.coordinates / data.metadata.a
-r = np.sqrt(
-    (posDM[:, 0] - xCen) ** 2 +
-    (posDM[:, 1] - yCen) ** 2 +
-    (posDM[:, 2] - zCen) ** 2
-) / R200c
+    # ZOOM #
+    # Load velociraptor data
+    with h5py.File(velociraptor_properties_zoom, 'r') as vr_file:
+        M200c = vr_file['/Mass_200crit'][0] * 1e10
+        R200c = vr_file['/R_200crit'][0]
+        Xcminpot = vr_file['/Xcminpot'][0]
+        Ycminpot = vr_file['/Ycminpot'][0]
+        Zcminpot = vr_file['/Zcminpot'][0]
 
-# Calculate particle mass and rho_crit
-unitLength = data.metadata.units.length
-unitMass = data.metadata.units.mass
-rho_crit = unyt.unyt_quantity(
-    data.metadata.cosmology['Critical density [internal units]'],
-    unitMass / unitLength ** 3
-)
-rhoMean = rho_crit * data.metadata.cosmology['Omega_m']
-vol = data.metadata.boxsize[0] ** 3
-numPart = data.metadata.n_dark_matter
-particleMass = rhoMean * vol / numPart
-parent_mass_resolution = particleMass
+    M200c = unyt.unyt_quantity(M200c, unyt.Solar_Mass)
+    R200c = unyt.unyt_quantity(R200c, unyt.Mpc)
+    xCen = unyt.unyt_quantity(Xcminpot, unyt.Mpc)
+    yCen = unyt.unyt_quantity(Ycminpot, unyt.Mpc)
+    zCen = unyt.unyt_quantity(Zcminpot, unyt.Mpc)
 
-# Construct bins and compute density profile
-lbins = np.logspace(-2, np.log10(out_to_radius), 40)
-hist, bin_edges = np.histogram(r, bins=lbins)
-bin_centre = np.sqrt(bin_edges[1:] * bin_edges[:-1])
-volume_shell = (4. * np.pi / 3.) * (R200c ** 3) * ((bin_edges[1:]) ** 3 - (bin_edges[:-1]) ** 3)
-densities = hist * particleMass / volume_shell / rho_crit
+    # Construct spatial mask to feed into swiftsimio
+    size = radius_bounds[1] * R200c
+    mask = sw.mask(snap_filepath_zoom)
+    region = [
+        [xCen - size, xCen + size],
+        [yCen - size, yCen + size],
+        [zCen - size, zCen + size]
+    ]
+    mask.constrain_spatial(region)
+    data = sw.load(snap_filepath_zoom, mask=mask)
 
-# Plot density profile for each selected halo in volume
-fig, ax = plt.subplots()
-parent_label = f'Parent: $m_\\mathrm{{DM}} = {latex_float(parent_mass_resolution.value[0])}\\ {parent_mass_resolution.units.latex_repr}$'
-ax.plot(bin_centre, densities, c="lime", linestyle="-", label=parent_label)
+    # Get DM particle coordinates and compute radial distance from CoP in R200 units
+    posDM = data.dark_matter.coordinates / data.metadata.a
+    r = np.sqrt(
+        (posDM[:, 0] - xCen) ** 2 +
+        (posDM[:, 1] - yCen) ** 2 +
+        (posDM[:, 2] - zCen) ** 2
+    ) / R200c
 
-##########################################################
-# ZOOM
+    # Calculate particle mass and rho_crit
+    unitLength = data.metadata.units.length
+    unitMass = data.metadata.units.mass
+    rho_crit = unyt.unyt_quantity(
+        data.metadata.cosmology['Critical density [internal units]'],
+        unitMass / unitLength ** 3
+    )
+    particleMasses = data.dark_matter.masses.to('Msun')
+    zoom_mass_resolution = particleMasses
 
-snap_filepath = f"/cosma6/data/dp004/rttw52/EAGLE-XL/EAGLE-XL_ClusterSK{halo_id}_DMO/snapshots/EAGLE-XL_ClusterSK{halo_id}_DMO_0001.hdf5"
-velociraptor_properties = f"/cosma6/data/dp004/dc-alta2/xl-zooms/halo_{author}{halo_id}_0001/halo_{author}{halo_id}_0001.properties.0"
+    # Construct bins and compute density profile
+    lbins = np.logspace(np.log10(radius_bounds[0]), np.log10(radius_bounds[1]), bins)
+    hist, bin_edges = np.histogram(r, bins=lbins, weights=particleMasses)
+    bin_centre = np.sqrt(bin_edges[1:] * bin_edges[:-1])
+    volume_shell = (4. * np.pi / 3.) * (R200c ** 3) * ((bin_edges[1:]) ** 3 - (bin_edges[:-1]) ** 3)
+    densities = hist / volume_shell / rho_crit
 
-# Load velociraptor data
-with h5py.File(velociraptor_properties, 'r') as vr_file:
-    M200c = vr_file['/Mass_200crit'][0] * 1e10
-    R200c = vr_file['/R_200crit'][0]
-    Xcminpot = vr_file['/Xcminpot'][0]
-    Ycminpot = vr_file['/Ycminpot'][0]
-    Zcminpot = vr_file['/Zcminpot'][0]
+    # Plot density profile for each selected halo in volume
+    zoom_label = f'Zoom: $m_\\mathrm{{DM}} = {latex_float(zoom_mass_resolution.value[0])}\\ {zoom_mass_resolution.units.latex_repr}$'
+    ax.plot(bin_centre, densities, c="orange", linestyle="-", label=zoom_label)
 
-M200c = unyt.unyt_quantity(M200c, unyt.Solar_Mass )
-R200c = unyt.unyt_quantity(R200c, unyt.Mpc)
-xCen = unyt.unyt_quantity(Xcminpot, unyt.Mpc)
-yCen = unyt.unyt_quantity(Ycminpot, unyt.Mpc)
-zCen = unyt.unyt_quantity(Zcminpot, unyt.Mpc)
+    ax.text(
+        0.025,
+        0.025,
+        (
+            f"Halo {halo_id:d} DMO\n"
+            f"$z={data.metadata.z:3.3f}$\n"
+            "Zoom VR output:\n"
+            f"$M_{{200c}}={latex_float(M200c.value)}\\ {M200c.units.latex_repr}$\n"
+            f"$R_{{200c}}={latex_float(R200c.value)}\\ {R200c.units.latex_repr}$"
+        ),
+        color="black",
+        ha="left",
+        va="bottom",
+        transform=ax.transAxes,
+    )
 
-# Construct spatial mask to feed into swiftsimio
-size = out_to_radius * R200c
-mask = sw.mask(snap_filepath)
-region = [
-    [xCen - size, xCen + size],
-    [yCen - size, yCen + size],
-    [zCen - size, zCen + size]
-]
-mask.constrain_spatial(region)
-data = sw.load(snap_filepath, mask=mask)
+    ax.set_xlim(radius_bounds[0], radius_bounds[1])
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.set_ylabel(r"$\rho_{DM}\ /\ \rho_c$")
+    ax.set_xlabel(r"$R\ /\ R_{200c}$")
+    plt.legend()
+    fig.tight_layout()
+    fig.savefig(f"{output_directory}/halo{halo_id}{author}_density_profile_compare.png")
+    plt.close(fig)
 
-# Get DM particle coordinates and compute radial distance from CoP in R200 units
-posDM = data.dark_matter.coordinates / data.metadata.a
-r = np.sqrt(
-    (posDM[:, 0] - xCen) ** 2 +
-    (posDM[:, 1] - yCen) ** 2 +
-    (posDM[:, 2] - zCen) ** 2
-) / R200c
+    return
 
-# Calculate particle mass and rho_crit
-unitLength = data.metadata.units.length
-unitMass = data.metadata.units.mass
-rho_crit = unyt.unyt_quantity(
-    data.metadata.cosmology['Critical density [internal units]'],
-    unitMass / unitLength ** 3
-)
-particleMasses = data.dark_matter.masses.to('Msun')
-zoom_mass_resolution = particleMasses
 
-# Construct bins and compute density profile
-lbins = np.logspace(-2, np.log10(out_to_radius), 40)
-hist, bin_edges = np.histogram(r, bins=lbins, weights=particleMasses)
-bin_centre = np.sqrt(bin_edges[1:] * bin_edges[:-1])
-volume_shell = (4. * np.pi / 3.) * (R200c ** 3) * ((bin_edges[1:]) ** 3 - (bin_edges[:-1]) ** 3)
-densities = hist / volume_shell / rho_crit
+if __name__ == "__main__":
+    import sys
 
-# Plot density profile for each selected halo in volume
-zoom_label = f'Zoom: $m_\\mathrm{{DM}} = {latex_float(zoom_mass_resolution.value[0])}\\ {zoom_mass_resolution.units.latex_repr}$'
-ax.plot(bin_centre, densities, c="orange", linestyle="-", label=zoom_label)
+    run_name = sys.argv[1]
+    run_directory = sys.argv[2]
+    snapshot_name = sys.argv[3]
+    output_path = sys.argv[4]
 
-ax.text(
-    0.025,
-    0.025,
-    (
-        f"Halo {halo_id:d} DMO\n"
-        f"$z={data.metadata.z:3.3f}$\n"
-        "Zoom VR output:\n"
-        f"$M_{{200c}}={latex_float(M200c.value)}\\ {M200c.units.latex_repr}$\n"
-        f"$R_{{200c}}={latex_float(R200c.value)}\\ {R200c.units.latex_repr}$"
-    ),
-    color="black",
-    ha="left",
-    va="bottom",
-    transform=ax.transAxes,
-)
+    # Manual inputs
+    halo_id = 0
+    author = "SK"
+    snap_filepath_parent = "/cosma7/data/dp004/jch/EAGLE-XL/DMONLY/Cosma7/L0300N0564/snapshots/EAGLE-XL_L0300N0564_DMONLY_0036.hdf5"
+    snap_filepath_zoom = f"/cosma6/data/dp004/rttw52/EAGLE-XL/EAGLE-XL_ClusterSK{halo_id}_DMO/snapshots/EAGLE-XL_ClusterSK{halo_id}_DMO_0001.hdf5"
+    velociraptor_properties_zoom = f"/cosma6/data/dp004/dc-alta2/xl-zooms/halo_{author}{halo_id}_0001/halo_{author}{halo_id}_0001.properties.0"
+    output_directory = "outfiles"
 
-ax.set_xlim(1e-2, out_to_radius)
-ax.set_xscale('log')
-ax.set_yscale('log')
-ax.set_ylabel(r"$\rho_{DM}\ /\ \rho_c$")
-ax.set_xlabel(r"$R\ /\ R_{200c}$")
-plt.legend()
-fig.tight_layout()
-fig.savefig(f"{output_directory}halo{halo_id}{author}_density_profile_compare.png")
-plt.close(fig)
+    density_profile_compare_plot(
+        halo_id,
+        author,
+        snap_filepath_parent=snap_filepath_parent,
+        snap_filepath_zoom=snap_filepath_zoom,
+        velociraptor_properties_zoom=velociraptor_properties_zoom,
+        output_directory=output_directory
+    )
