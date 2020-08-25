6a9f8a7615cc1c484f55a40f691f4adaefbbc5e8
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-08-25 23:19:58 +0100

    VR on high res

--- zooms/compare_cumulative_mass.py
+++ zooms/compare_cumulative_mass.py
@@ -163,7 +163,7 @@ def cumulative_mass_compare_plot(
     if snap_filepath_zoom:
 
         # Set-up colors
-        cmap_discrete = plt.cm.get_cmap(cmap_name, len(velociraptor_properties_zoom))
+        cmap_discrete = plt.cm.get_cmap(cmap_name, len(velociraptor_properties_zoom)+10)
         cmaplist = [cmap_discrete(i) for i in range(cmap_discrete.N)]
 
         for snap_path, vrprop_path, color in zip(snap_filepath_zoom, velociraptor_properties_zoom, cmaplist):
