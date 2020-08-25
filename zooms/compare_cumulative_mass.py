793c7467a9dbc039f7e024f82760c6b449680c92
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-08-25 23:20:43 +0100

    VR on high res

--- zooms/compare_cumulative_mass.py
+++ zooms/compare_cumulative_mass.py
@@ -21,7 +21,7 @@ from convergence_radius import convergence_radius
 # Constants
 bins = 40
 radius_bounds = [1e-2, 3]  # In units of R200crit
-cmap_name = 'BuPu'
+cmap_name = 'BuPu_r'
 
 
 def latex_float(f):
