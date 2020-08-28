00f058e749bc4d95ffdfb177f4070d0d223d99ff
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-08-28 17:51:14 +0100

    Ic gen

--- performance/performance.sh
+++ performance/performance.sh
@@ -3,25 +3,25 @@ run_directory="/cosma6/data/dp004/rttw52/EAGLE-XL/EAGLE-XL_ClusterSK1_DMO"
 snapshot_name="snapshots/EAGLE-XL_ClusterSK1_DMO_0001.hdf5"
 output_path="outfiles"
 
-python3 performance/number_of_steps_simulation_time.py \
+python3 number_of_steps_simulation_time.py \
   $run_name \
   $run_directory \
   $snapshot_name \
   $output_path
 
-python3 performance/particle_updates_step_cost.py \
+python3 particle_updates_step_cost.py \
   $run_name \
   $run_directory \
   $snapshot_name \
   $output_path
 
-python3 performance/wallclock_number_of_steps.py \
+python3 wallclock_number_of_steps.py \
   $run_name \
   $run_directory \
   $snapshot_name \
   $output_path
 
-python3 performance/wallclock_simulation_time.py \
+python3 wallclock_simulation_time.py \
   $run_name \
   $run_directory \
   $snapshot_name \
