659c48594cbf36ea8c7ea5551615778b255c954d
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-02 18:11:40 +0100

    Ic gen

---
+++ migrate_source.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+destination_directory=/cosma7/data/dp004/dc-alta2/xl-zooms
+cp -r ./swift $destination_directory
+cp -r ./velociraptor $destination_directory
+cp modules.sh $destination_directory
