25432c5e32ee7e1b2006f83a3d0b487e1fbfc1db
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-03 00:07:41 +0100

    Ic gen

--- migrate_source.sh
+++ migrate_source.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 destination_directory=/cosma7/data/dp004/dc-alta2/xl-zooms
-cp -r ./swift $destination_directory
-cp -r ./velociraptor $destination_directory
-cp modules.sh $destination_directory
+subdirectory=HYDRO
+run_name=EAGLE-XL_ClusterSK0
+
+mkdir -p destination_directory/$subdirectory
+mkdir -p destination_directory/$subdirectory/$run_name
+mkdir -p destination_directory/$subdirectory/$run_name/logs
+
+cp ./swift/parameters_hydro.yml destination_directory/$subdirectory/$run_name
+cp ./swift/run_hydro.slurm destination_directory/$subdirectory/$run_name
+cp ./swift/snap_redshifts.txt destination_directory/$subdirectory/$run_name
+
+sbatch destination_directory/$subdirectory/$run_name/run_hydro.slurm
