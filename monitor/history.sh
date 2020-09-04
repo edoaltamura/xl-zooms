819d0962d84e288eacf5321dd795f2f7b1cfa28d
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-04 16:24:07 +0100

    VR run script

--- monitor/history.sh
+++ monitor/history.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
 
-sacct -u dc-alta2 --format=JobID,Jobname%40,partition,state,time,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,nodelist,CPUTime,AveRSS,MaxRSS,ExitCode
+sacct -u dc-alta2 --format=JobID,Jobname%40,partition,state,time,Submit,start,end,elapsed,MaxRss,MaxVMSize,nnodes,ncpus,nodelist,CPUTime,AveRSS,MaxRSS,ExitCode
