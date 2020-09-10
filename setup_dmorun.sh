95d4c349c7a66e55f98fa66e609c51c135911386
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-10 17:57:54 +0100

    DMO runs

--- setup_dmorun.sh
+++ setup_dmorun.sh
@@ -56,12 +56,12 @@ cp $old_directory/swift/vr_redshifts.txt ./config
 cp $old_directory/velociraptor/standalone/run_vr.slurm .
 cp $old_directory/velociraptor/interface-swift/vrconfig_3dfof_subhalos_SO_hydro.cfg ./config
 cp /cosma7/data/dp004/rttw52/swift_runs/make_ics/ics/EAGLE-XL_ClusterSK0.hdf5 ./ics
-mv ./ics/EAGLE-XL_ClusterSK0.hdf5.hdf5 ./ics/$run_name.hdf5
+mv ./ics/EAGLE-XL_ClusterSK0.hdf5 ./ics/$run_name.hdf5
 
 # Edit run names in the submission and parameter files
 sed -i "s/RUN_NAME/$run_name/" ./run_swift.slurm
 sed -i "s/RUN_NAME/$run_name/" ./run_vr.slurm
-sed -i "s/RUN_NAME/$run_name/" ./config/eagle_$resolution.yml
+sed -i "s/RUN_NAME/$run_name/" ./config/dmo_$resolution.yml
 sed -i "s/RUN_NAME/$run_name/" ./README.md
 
 sed -i "s/PARAM_FILE/dmo_$resolution/" ./run_swift.slurm
