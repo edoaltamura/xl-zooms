a2f8845f655119f099dd429da99545c8c2c05eb3
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-09-10 14:34:01 +0100

    Fix to timestep

--- modules.sh
+++ modules.sh
@@ -3,7 +3,7 @@ module purge
 module load intel_comp/2018
 module load intel_mpi/2018
 module load parmetis/4.0.3
-module load parallel_hdf5/1.10.6
+module load parallel_hdf5/1.10.5
 module load gsl/2.4
 module load fftw/3.3.7
 module load python/3.6.5
