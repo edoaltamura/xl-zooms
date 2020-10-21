f801cbfd22bf070aad6061a817068ff437880e24
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-10-21 16:25:05 +0100

    Integrate pipeline

--- diagnostics-pipeline/visualise_dm.py
+++ diagnostics-pipeline/visualise_dm.py
@@ -107,11 +107,11 @@ def dm_map_parent(
     fig.subplots_adjust(0, 0, 1, 1)
     ax.axis("off")
     ax.imshow(dm_mass.T, norm=LogNorm(), cmap="inferno", origin="lower", extent=(region[0] + region[1]))
-    ax.text(
+    info = ax.text(
         0.025,
         0.025,
         (
-            f"Halo {run_name:s} DMO\n"
+            f"Halo {run_name:s} DMO - parent\n"
             f"$z={data.metadata.z:3.3f}$\n"
             f"$M_{{200c}}={latex_float(M200c.value)}\\ {M200c.units.latex_repr}$\n"
             f"$R_{{200c}}={latex_float(R200c.value)}\\ {R200c.units.latex_repr}$\n"
@@ -120,28 +120,28 @@ def dm_map_parent(
         color="white",
         ha="left",
         va="bottom",
-        backgroundcolor='black',
-        alpha=0.5,
+        alpha=0.8,
         transform=ax.transAxes,
     )
+    info.set_bbox(dict(facecolor='black', alpha=0.2, edgecolor='grey'))
     ax.text(
-        0,
-        0 + 1.05 * R200c,
+        xCen,
+        yCen + 1.05 * R200c,
         r"$R_{200c}$",
         color="black",
         ha="center",
         va="bottom"
     )
     ax.text(
-        0,
-        0 + 1.02 * _highres_radius,
+        xCen,
+        yCen + 1.02 * _highres_radius,
         r"$R_\mathrm{clean}$",
-        color="red",
+        color="white",
         ha="center",
         va="bottom"
     )
     circle_r200 = plt.Circle((xCen, yCen), R200c, color="black", fill=False, linestyle='-')
-    circle_clean = plt.Circle((xCen, yCen), _highres_radius.value, color="red", fill=False, linestyle=':')
+    circle_clean = plt.Circle((xCen, yCen), _highres_radius.value, color="white", fill=False, linestyle=':')
     ax.add_artist(circle_r200)
     ax.add_artist(circle_clean)
     ax.set_xlim([xCen.value - size.value, xCen.value + size.value])
@@ -155,12 +155,97 @@ def dm_map_parent(
 
 def dm_map_zoom(
         run_name: str,
-        velociraptor_properties_zoom: str,
         snap_filepath_zoom: str,
+        velociraptor_properties_zoom: str,
         out_to_radius: Tuple[int, str] = (5, 'R200c'),
         highres_radius: Tuple[int, str] = (6, 'R500c'),
         output_directory: str = '.'
 ) -> None:
+    print(f"Rendering {snap_filepath_zoom}...")
+
+    # Rendezvous over parent VR catalogue using zoom information
+    with h5py.File(velociraptor_properties_zoom, 'r') as vr_file:
+
+        M200c = unyt.unyt_quantity(vr_file['/Mass_200crit'][0] * 1e10, unyt.Solar_Mass)
+        R200c = unyt.unyt_quantity(vr_file['/R_200crit'][0], unyt.Mpc)
+        R500c = unyt.unyt_quantity(vr_file['/SO_R_500_rhocrit'][0], unyt.Mpc)
+        xCen = unyt.unyt_quantity(vr_file['/Xcminpot'][0], unyt.Mpc)
+        yCen = unyt.unyt_quantity(vr_file['/Ycminpot'][0], unyt.Mpc)
+        zCen = unyt.unyt_quantity(vr_file['/Zcminpot'][0], unyt.Mpc)
+
+    if out_to_radius[1] == 'R200c':
+        size = out_to_radius[0] * R200c
+    elif out_to_radius[1] == 'R500c':
+        size = out_to_radius[0] * R500c
+    elif out_to_radius[1] == 'Mpc' or out_to_radius[1] is None:
+        size = unyt.unyt_quantity(out_to_radius[0], unyt.Mpc)
+
+    if highres_radius[1] == 'R200c':
+        _highres_radius = highres_radius[0] * R200c
+    elif highres_radius[1] == 'R500c':
+        _highres_radius = highres_radius[0] * R500c
+    elif highres_radius[1] == 'Mpc' or highres_radius[1] is None:
+        _highres_radius = unyt.unyt_quantity(highres_radius[0], unyt.Mpc)
+
+    # Construct spatial mask to feed into swiftsimio
+    mask = sw.mask(snap_filepath_zoom)
+    region = [
+        [xCen - size, xCen + size],
+        [yCen - size, yCen + size],
+        [zCen - size, zCen + size]
+    ]
+    mask.constrain_spatial(region)
+    data = sw.load(snap_filepath_zoom, mask=mask)
+    dm_mass = dm_render(data, region=(region[0] + region[1]))
+
+    # Make figure
+    fig, ax = plt.subplots(figsize=(8, 8), dpi=1024 // 8)
+    fig.subplots_adjust(0, 0, 1, 1)
+    ax.axis("off")
+    ax.imshow(dm_mass.T, norm=LogNorm(), cmap="inferno", origin="lower", extent=(region[0] + region[1]))
+    info = ax.text(
+        0.025,
+        0.025,
+        (
+            f"Halo {run_name:s} DMO - zoom\n"
+            f"$z={data.metadata.z:3.3f}$\n"
+            f"$M_{{200c}}={latex_float(M200c.value)}\\ {M200c.units.latex_repr}$\n"
+            f"$R_{{200c}}={latex_float(R200c.value)}\\ {R200c.units.latex_repr}$\n"
+            f"$R_\\mathrm{{clean}}={highres_radius[0]}\\ {highres_radius[1]}$"
+        ),
+        color="white",
+        ha="left",
+        va="bottom",
+        alpha=0.8,
+        transform=ax.transAxes,
+    )
+    info.set_bbox(dict(facecolor='black', alpha=0.2, edgecolor='grey'))
+    ax.text(
+        xCen,
+        yCen + 1.05 * R200c,
+        r"$R_{200c}$",
+        color="black",
+        ha="center",
+        va="bottom"
+    )
+    ax.text(
+        xCen,
+        yCen + 1.02 * _highres_radius,
+        r"$R_\mathrm{clean}$",
+        color="white",
+        ha="center",
+        va="bottom"
+    )
+    circle_r200 = plt.Circle((xCen, yCen), R200c, color="black", fill=False, linestyle='-')
+    circle_clean = plt.Circle((xCen, yCen), _highres_radius.value, color="white", fill=False, linestyle=':')
+    ax.add_artist(circle_r200)
+    ax.add_artist(circle_clean)
+    ax.set_xlim([xCen.value - size.value, xCen.value + size.value])
+    ax.set_ylim([yCen.value - size.value, yCen.value + size.value])
+    fig.savefig(f"{output_directory}/{run_name}_dark_matter_map_zoom.png")
+    plt.close(fig)
+    print(f"Saved: {output_directory}/{run_name}_dark_matter_map_zoom.png")
+
     return
 
 
@@ -190,6 +275,15 @@ if __name__ == "__main__":
         run_name=run_name,
         velociraptor_properties_parent=velociraptor_properties_parent,
         snap_filepath_parent=snap_filepath_parent,
+        velociraptor_properties_zoom=velociraptor_properties_zoom,
+        out_to_radius=out_to_radius,
+        highres_radius=highres_radius,
+        output_directory=output_directory,
+    )
+
+    dm_map_parent(
+        run_name=run_name,
+        snap_filepath_zoom=snap_filepath_zoom,
         velociraptor_properties_zoom=velociraptor_properties_zoom,
         out_to_radius=out_to_radius,
         highres_radius=highres_radius,
