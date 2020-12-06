85266969d918132dc4eddb11a9c4051fb6ffa32e
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-12-06 22:43:01 +0000

    Obs line weightings

--- zooms/radial_profiles.py
+++ zooms/radial_profiles.py
@@ -149,17 +149,18 @@ def profile_3d_single_halo(path_to_snap: str, path_to_catalogue: str, weights: s
         ylabel = r'$(k_B T/k_B T_{500{\rm crit}})$'
 
     elif weights.lower() == 'entropy':
-        n_e = data.gas.densities / (mean_atomic_weight_per_free_electron * unyt.mass_proton)
+        n_e = (data.gas.densities / (mean_atomic_weight_per_free_electron * unyt.mass_proton)).to('m**-3')
         weights_field = data.gas.mass_weighted_temperatures / n_e ** (2 / 3)
         hist, _ = histogram_unyt(radial_distance, bins=lbins, weights=weights_field)
         hist /= mass_weights
+        hist.to('keV * m**2')
 
         # Make dimensionless, divide by K_500crit
         # norm = unyt.unyt_quantity(2.25342209e-23, 'Mpc**4/(Gyr**2*Msun**(5/3))')
         kBT = unyt.G * mean_molecular_weight * M500c * unyt.mass_proton / 2 / R500c
-        ne_500crit = 3 * M500c * fbary / (4 * np.pi * R500c ** 3) / (mean_atomic_weight_per_free_electron * unyt.mass_proton)
+        ne_500crit = (3 * M500c * fbary / (4 * np.pi * R500c ** 3) / (mean_atomic_weight_per_free_electron * unyt.mass_proton)).to('m**-3')
         norm = kBT / ne_500crit ** (2 / 3)
-        hist /= norm.value
+        hist /= norm.to('keV * m**2')
 
         ylabel = r'$(K/K_{500{\rm crit}})$'
 
