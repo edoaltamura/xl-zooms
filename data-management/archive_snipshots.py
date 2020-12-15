492abae37c08972a79fb2e2872951973f5edaf9e
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-12-15 23:35:56 +0100

    Introduce Voit 2005

--- data-management/archive_snipshots.py
+++ data-management/archive_snipshots.py
@@ -2,6 +2,7 @@ import os
 import zipfile
 import shutil
 import numpy as np
+from tqdm import trange
 
 
 def humanize_bytes(num, suffix='B'):
@@ -48,13 +49,12 @@ def compress_snipshots(run_directory: str):
     archive_filename = os.path.join(run_directory, "snapshots", "snipshots.zip")
     with zipfile.ZipFile(archive_filename, 'w', zipfile.ZIP_DEFLATED) as zip_handle:
         file_sizes = np.ones_like(snipshots_filenames, dtype=np.int64)
-        for i, file in enumerate(snipshots_filenames):
-            print(file)
-            file = os.path.join(run_directory, 'snapshots', file)
+        for i in trange(len(snipshots_filenames), desc=f"Compression to snipshots.zip"):
+            file = os.path.join(run_directory, 'snapshots', snipshots_filenames[i])
             size = os.path.getsize(file)
             file_sizes[i] = size
             zip_handle.write(file)
-            # os.remove(file)
+            os.remove(file)
 
     print(
         "Compression complete.\n"
@@ -63,8 +63,6 @@ def compress_snipshots(run_directory: str):
         f"Total file size: {humanize_bytes(np.sum(file_sizes))}\n"
         f"Archive file size: {humanize_bytes(os.path.getsize(archive_filename))}\n"
     )
-
-    return
 
 
 def remove_restart_files(run_directory: str):
@@ -79,8 +77,15 @@ def extract_snipshots(run_directory: str):
     with zipfile.ZipFile(archive_filename, 'r') as zip_handle:
         zip_handle.extractall(os.path.join(run_directory, "snapshots", "snipshots"))
 
+def remove_extracted_snipshots(run_directory: str):
+    if os.path.isdir(os.path.join(run_directory, "snapshots", "snipshots")):
+        shutil.rmtree(os.path.join(run_directory, "snapshots", "snipshots"))
+    else:
+        print("No restart directory found. Nothing was removed.")
+
 
 if __name__ == '__main__':
+    remove_extracted_snipshots("/cosma/home/dp004/dc-alta2/snap7/xl-zooms/hydro/L0300N0564_VR3032_-8res_Isotropic")
     compress_snipshots("/cosma/home/dp004/dc-alta2/snap7/xl-zooms/hydro/L0300N0564_VR3032_-8res_Isotropic")
     remove_restart_files("/cosma/home/dp004/dc-alta2/snap7/xl-zooms/hydro/L0300N0564_VR3032_-8res_Isotropic")
     extract_snipshots("/cosma/home/dp004/dc-alta2/snap7/xl-zooms/hydro/L0300N0564_VR3032_-8res_Isotropic")
