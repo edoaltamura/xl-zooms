b9a9db5150ba45bb3fd2e42298cd3484f33b2651
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-12-16 00:17:19 +0100

    Introduce Voit 2005

--- data-management/archive_snipshots.py
+++ data-management/archive_snipshots.py
@@ -96,7 +96,7 @@ def remove_extracted_snipshots(run_directory: str):
 
 if __name__ == '__main__':
     working_run_directory = "/cosma/home/dp004/dc-alta2/snap7/xl-zooms/hydro/L0300N0564_VR3032_-8res_Isotropic"
-    # remove_extracted_snipshots(working_run_directory)
-    # compress_snipshots(working_run_directory)
-    # remove_restart_files(working_run_directory)
+    remove_extracted_snipshots(working_run_directory)
+    compress_snipshots(working_run_directory)
+    remove_restart_files(working_run_directory)
     extract_snipshots(working_run_directory)
