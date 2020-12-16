4a38cbf9793cfc8271c27390fa95fd17aa1dd895
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-12-16 23:36:09 +0100

    Load hotgas comparison

--- zooms/radial_profiles.py
+++ zooms/radial_profiles.py
@@ -251,7 +251,7 @@ def profile_3d_single_halo(path_to_snap: str, path_to_catalogue: str, weights: s
 
 if __name__ == "__main__":
 
-    vr_num = 'Minimum'
+    vr_num = 'Isotropic'
     field_name = 'entropy_physical'
 
 
