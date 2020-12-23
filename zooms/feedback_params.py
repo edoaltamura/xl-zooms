1dc434d24923e336078c5f75aebfe187d6c52a64
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2020-12-23 17:52:15 +0100

    Import snipshots from zip

--- zooms/feedback_params.py
+++ zooms/feedback_params.py
@@ -1,6 +1,7 @@
 import unyt
 import os
 import numpy as np
+from tqdm import tqdm
 from collections import defaultdict
 from multiprocessing import Pool, cpu_count
 import h5py as h5
@@ -11,6 +12,7 @@ from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
 
 from register import (
+    SILENT_PROGRESSBAR,
     zooms_register,
     Zoom,
     Tcut_halogas,
@@ -46,7 +48,7 @@ def latex_float(f):
 
 
 def feedback_stats_dT(path_to_snap: str, path_to_catalogue: str) -> dict:
-    get_snip_handles(path_to_snap)
+
 
     # Read in halo properties
     with h5.File(f'{path_to_catalogue}', 'r') as h5file:
@@ -79,7 +81,10 @@ def feedback_stats_dT(path_to_snap: str, path_to_catalogue: str) -> dict:
     central_bh['x'] = []
     central_bh['y'] = []
     central_bh['z'] = []
-    central_bh['r'] = []
+    central_bh['dx'] = []
+    central_bh['dy'] = []
+    central_bh['dz'] = []
+    central_bh['dr'] = []
     central_bh['mass'] = []
     central_bh['m500c'] = []
     central_bh['id'] = []
@@ -120,9 +125,9 @@ def feedback_stats_dT(path_to_snap: str, path_to_catalogue: str) -> dict:
 
         data = sw.load(highz_snap)
         bh_positions = data.black_holes.coordinates.to_physical()
-        bh_coordX = (bh_positions[:, 0] - XPotMin)
-        bh_coordY = (bh_positions[:, 1] - YPotMin)
-        bh_coordZ = (bh_positions[:, 2] - ZPotMin)
+        bh_coordX = bh_positions[:, 0] - XPotMin
+        bh_coordY = bh_positions[:, 1] - YPotMin
+        bh_coordZ = bh_positions[:, 2] - ZPotMin
         bh_radial_distance = np.sqrt(bh_coordX ** 2 + bh_coordY ** 2 + bh_coordZ ** 2)
 
         if BH_LOCK_ID:
@@ -130,15 +135,50 @@ def feedback_stats_dT(path_to_snap: str, path_to_catalogue: str) -> dict:
         else:
             central_bh_index = np.argmin(bh_radial_distance)
 
-        central_bh['x'].append(bh_coordX[central_bh_index])
-        central_bh['y'].append(bh_coordY[central_bh_index])
-        central_bh['z'].append(bh_coordZ[central_bh_index])
-        central_bh['r'].append(bh_radial_distance[central_bh_index])
+        central_bh['x'].append(bh_positions[central_bh_index, 0])
+        central_bh['y'].append(bh_positions[central_bh_index, 1])
+        central_bh['z'].append(bh_positions[central_bh_index, 2])
+        central_bh['dx'].append(bh_coordX[central_bh_index])
+        central_bh['dy'].append(bh_coordY[central_bh_index])
+        central_bh['dz'].append(bh_coordZ[central_bh_index])
+        central_bh['dr'].append(bh_radial_distance[central_bh_index])
         central_bh['mass'].append(data.black_holes.dynamical_masses.to_physical()[central_bh_index])
         central_bh['m500c'].append(M500c)
         central_bh['id'].append(data.black_holes.particle_ids[central_bh_index])
         central_bh['redshift'].append(data.metadata.redshift)
         central_bh['time'].append(data.metadata.time)
+
+    if INCLUDE_SNIPS:
+        snip_handles = get_snip_handles(path_to_snap, z_max=z_clip)
+        for snip_handle in tqdm(snip_handles, desc=f"Analysing snipshots", disable=SILENT_PROGRESSBAR):
+
+            data = sw.load(snip_handle)
+            bh_positions = data.black_holes.coordinates.to_physical()
+            bh_masses = data.black_holes.dynamical_masses.to_physical()
+
+            if BH_LOCK_ID:
+                central_bh_index = np.where(data.black_holes.particle_ids.v == central_bh_id_target.v)[0]
+            else:
+                raise ValueError((
+                    "Trying to lock the central BH to the halo centre of potential "
+                    "in snipshots, which do not have corresponding halo catalogues. "
+                    "Please, lock the central BH to the particle ID found at z = 0. "
+                    "While this set-up is not yet implemented, it would be possible to "
+                    "interpolate the position of the CoP between snapshots w.r.t. cosmic time."
+                ))
+
+            central_bh['x'].append(bh_positions[central_bh_index, 0])
+            central_bh['y'].append(bh_positions[central_bh_index, 1])
+            central_bh['z'].append(bh_positions[central_bh_index, 2])
+            central_bh['dx'].append(np.nan)
+            central_bh['dy'].append(np.nan)
+            central_bh['dz'].append(np.nan)
+            central_bh['dr'].append(np.nan)
+            central_bh['mass'].append(bh_masses[central_bh_index])
+            central_bh['m500c'].append(np.nan)
+            central_bh['id'].append(data.black_holes.particle_ids[central_bh_index])
+            central_bh['redshift'].append(data.metadata.redshift)
+            central_bh['time'].append(data.metadata.time)
 
     # Convert lists to Swiftsimio cosmo arrays
     for key in central_bh:
@@ -162,7 +202,7 @@ if __name__ == "__main__":
     central_bh['mass'] = central_bh['mass'].to('Solar_Mass')
 
     fig, ax1 = plt.subplots()
-    ax1.plot(central_bh['time'], central_bh['mass'])
+    ax1.scatter(central_bh['time'], central_bh['mass'], marker='.', s=3, alpha=1, edgecolors='none')
     ax1.set_xlabel(r"Cosmic time [${}$]".format(central_bh['time'].units.latex_repr))
     ax1.set_ylabel(r"BH dynamical mass [${}$]".format(central_bh['mass'].units.latex_repr))
     ax1.set_yscale('log')
