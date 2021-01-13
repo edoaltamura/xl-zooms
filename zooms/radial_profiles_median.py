189c7a0d652eb01bbbfff587ec6f0c95d1797e3f
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-13 20:27:15 +0100

    k scaling relations

--- zooms/radial_profiles_median.py
+++ zooms/radial_profiles_median.py
@@ -172,7 +172,7 @@ def plot_radial_profiles_median(object_database: pd.DataFrame, bin_edges: np.nda
 
 if __name__ == "__main__":
 
-    keyword = ''
+    keyword = 'fixedAGNdT9.5'
 
     try:
         results_database = load_catalogue(find_keyword=keyword)
