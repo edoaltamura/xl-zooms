dfcb91c09513a0c8e5a0bea0c7aeec8067958a24
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-24 15:23:49 +0100

    k scaling relations

--- scaling_relations/hotgas_halomass.py
+++ scaling_relations/hotgas_halomass.py
@@ -101,13 +101,15 @@ def m_500_hotgas(results: pd.DataFrame):
             results.loc[i, "Mhot500c"],
             marker=run_style['Marker style'],
             c=run_style['Color'],
-            alpha=0.7,
             s=run_style['Marker size'],
-            edgecolors='none', zorder=5
+            alpha=1,
+            edgecolors='none',
+            zorder=5
         )
 
     # Build legends
     legend_sims = plt.legend(handles=legend_handles, loc=2)
+    ax.add_artist(legend_sims)
 
     # Display observational data
     Sun09 = obs.Sun09()
@@ -126,7 +128,6 @@ def m_500_hotgas(results: pd.DataFrame):
     ]
     del Sun09, Lovisari15
     legend_obs = plt.legend(handles=handles, loc=4)
-    ax.add_artist(legend_sims)
     ax.add_artist(legend_obs)
 
     ax.set_xlabel(r'$M_{500{\rm crit}}\ [{\rm M}_{\odot}]$')
@@ -140,6 +141,63 @@ def m_500_hotgas(results: pd.DataFrame):
     plt.close()
 
 
+def f_500_hotgas(results: pd.DataFrame):
+    fig, ax = plt.subplots()
+    legend_handles = []
+    for i in range(len(results)):
+
+        run_style = style.get_style_for_object(results.loc[i, "Run name"])
+        if run_style['Legend handle'] not in legend_handles:
+            legend_handles.append(run_style['Legend handle'])
+
+        ax.scatter(
+            results.loc[i, "M_500crit"],
+            results.loc[i, "fhot500c"],
+            marker=run_style['Marker style'],
+            c=run_style['Color'],
+            s=run_style['Marker size'],
+            alpha=1,
+            edgecolors='none',
+            zorder=5
+        )
+
+    # Build legends
+    legend_sims = plt.legend(handles=legend_handles, loc=2)
+    ax.add_artist(legend_sims)
+
+    # Display observational data
+    Sun09 = obs.Sun09()
+    Lovisari15 = obs.Lovisari15()
+    ax.scatter(Sun09.M500, Sun09.Mgas500 / Sun09.M500,
+               marker='d', s=8, alpha=1,
+               color=(0.65, 0.65, 0.65), edgecolors='none', zorder=0)
+    ax.scatter(Lovisari15.M500, Lovisari15.Mgas500 / Lovisari15.M500,
+               marker='s', s=8, alpha=1,
+               color=(0.65, 0.65, 0.65), edgecolors='none', zorder=0)
+
+    handles = [
+        Line2D([], [], color=(0.65, 0.65, 0.65), marker='d', markeredgecolor='none', linestyle='None', markersize=4,
+               label=Sun09.paper_name),
+        Line2D([], [], color=(0.65, 0.65, 0.65), marker='s', markeredgecolor='none', linestyle='None', markersize=4,
+               label=Lovisari15.paper_name),
+        Line2D([], [], color='black', linestyle='--', markersize=0, label=f"Planck18 $f_{{bary}}=${fbary:.3f}"),
+    ]
+    del Sun09, Lovisari15
+    legend_obs = plt.legend(handles=handles, loc=4)
+    ax.add_artist(legend_obs)
+
+    ax.set_xlabel(r'$M_{500{\rm crit}}\ [{\rm M}_{\odot}]$')
+    ax.set_ylabel(r'$M_{{\rm gas},500{\rm crit}}\ [{\rm M}_{\odot}]$')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    ax.plot(ax.get_xlim(), [fbary for lim in ax.get_xlim()], '--', color='k')
+
+    fig.savefig(f'{zooms_register[0].output_directory}/m_500_hotgas.png', dpi=300)
+    plt.show()
+    plt.close()
+
+
 if __name__ == "__main__":
     results = utils.process_catalogue(_process_single_halo, find_keyword='Ref')
     m_500_hotgas(results)
+    f_500_hotgas(results)
--- scaling_relations/starmass_halomass.py
+++ scaling_relations/starmass_halomass.py
@@ -17,6 +17,7 @@ sys.path.append("../observational_data")
 from register import zooms_register, Zoom, Tcut_halogas, name_list
 import observational_data as obs
 import scaling_utils as utils
+import scaling_style as style
 
 try:
     plt.style.use("../mnras.mplstyle")
@@ -56,50 +57,29 @@ def _process_single_halo(zoom: Zoom):
     return process_single_halo(zoom.snapshot_file, zoom.catalog_file)
 
 
-def m_500_star():
+def m_500_star(results: pd.DataFrame):
     fig, ax = plt.subplots()
-
-    # The results of the multiprocessing Pool are returned in the same order as inputs
-    with Pool() as pool:
-        print(f"Analysis mapped onto {cpu_count():d} CPUs.")
-        results = pool.map(_process_single_halo, iter(zooms_register))
-
-        # Recast output into a Pandas dataframe for further manipulation
-        columns = [
-            'M_500crit (M_Sun)',
-            'M_star (< R_500crit) (M_Sun)',
-            'M_hot (< R_500crit) (M_Sun)',
-        ]
-        results = pd.DataFrame(list(results), columns=columns)
-        results.insert(0, 'Run name', pd.Series(name_list, dtype=str))
-        print(results)
-
-    # Display zoom data
+    legend_handles = []
     for i in range(len(results)):
 
-        marker = ''
-        if '-8res' in results.loc[i, "Run name"]:
-            marker = '.'
-        elif '+1res' in results.loc[i, "Run name"]:
-            marker = '^'
-
-        color = ''
-        if 'Ref' in results.loc[i, "Run name"]:
-            color = 'black'
-        elif 'MinimumDistance' in results.loc[i, "Run name"]:
-            color = 'orange'
-        elif 'Isotropic' in results.loc[i, "Run name"]:
-            color = 'lime'
-
-        markersize = 14
-        if marker == '.':
-            markersize *= 1.5
+        run_style = style.get_style_for_object(results.loc[i, "Run name"])
+        if run_style['Legend handle'] not in legend_handles:
+            legend_handles.append(run_style['Legend handle'])
 
         ax.scatter(
-            results.loc[i, "M_500crit (M_Sun)"],
-            results.loc[i, "M_star (< R_500crit) (M_Sun)"],
-            marker=marker, c=color, alpha=0.5, s=markersize, edgecolors='none', zorder=5
+            results.loc[i, "M_500crit"],
+            results.loc[i, "Mstar500c"],
+            marker=run_style['Marker style'],
+            c=run_style['Color'],
+            s=run_style['Marker size'],
+            alpha=1,
+            edgecolors='none',
+            zorder=5
         )
+
+    # Build legends
+    legend_sims = plt.legend(handles=legend_handles, loc=2)
+    ax.add_artist(legend_sims)
 
     # Display observational data
     Budzynski14 = obs.Budzynski14()
@@ -110,17 +90,6 @@ def m_500_star():
     ax.scatter(Kravtsov18.M500, Kravtsov18.Mstar500, marker='*', s=12, color=(0.85, 0.85, 0.85),
                linewidth=1.2, edgecolors='none', zorder=0)
 
-    # Build legends
-    handles = [
-        Line2D([], [], marker='.', markeredgecolor='black', markerfacecolor='none', markeredgewidth=1,
-               linestyle='None', markersize=6, label='-8 Res'),
-        Line2D([], [], marker='^', markeredgecolor='black', markerfacecolor='none', markeredgewidth=1,
-               linestyle='None', markersize=3, label='+1 Res'),
-        Patch(facecolor='black', edgecolor='None', label='Random (Ref)'),
-        Patch(facecolor='orange', edgecolor='None', label='Minimum distance'),
-        Patch(facecolor='lime', edgecolor='None', label='Isotropic'),
-    ]
-    legend_sims = plt.legend(handles=handles, loc=2)
     handles = [
         Line2D([], [], color=(0.85, 0.85, 0.85), marker='.', markeredgecolor='none', linestyle='-', markersize=0,
                label=Budzynski14.paper_name),
@@ -129,7 +98,6 @@ def m_500_star():
     ]
     del Budzynski14, Kravtsov18
     legend_obs = plt.legend(handles=handles, loc=4)
-    ax.add_artist(legend_sims)
     ax.add_artist(legend_obs)
 
     ax.set_xlabel(r'$M_{500{\rm crit}}\ [{\rm M}_{\odot}]$')
@@ -140,193 +108,7 @@ def m_500_star():
     plt.show()
     plt.close()
 
-    return
 
-
-def f_500_star():
-    fig, ax = plt.subplots()
-
-    # The results of the multiprocessing Pool are returned in the same order as inputs
-    with Pool() as pool:
-        print(f"Analysis mapped onto {cpu_count():d} CPUs.")
-        results = pool.map(_process_single_halo, iter(zooms_register))
-
-        # Recast output into a Pandas dataframe for further manipulation
-        columns = [
-            'M_500crit (M_Sun)',
-            'M_star (< R_500crit) (M_Sun)',
-            'M_hot (< R_500crit) (M_Sun)',
-        ]
-        results = pd.DataFrame(list(results), columns=columns)
-        results.insert(0, 'Run name', pd.Series(name_list, dtype=str))
-        print(results)
-
-    # Display zoom data
-    for i in range(len(results)):
-
-        marker = ''
-        if '-8res' in results.loc[i, "Run name"]:
-            marker = '.'
-        elif '+1res' in results.loc[i, "Run name"]:
-            marker = '^'
-
-        color = ''
-        if 'Ref' in results.loc[i, "Run name"]:
-            color = 'black'
-        elif 'MinimumDistance' in results.loc[i, "Run name"]:
-            color = 'orange'
-        elif 'Isotropic' in results.loc[i, "Run name"]:
-            color = 'lime'
-
-        markersize = 14
-        if marker == '.':
-            markersize *= 1.5
-
-        ax.scatter(
-            results.loc[i, "M_500crit (M_Sun)"],
-            results.loc[i, "M_star (< R_500crit) (M_Sun)"] / results.loc[i, "M_500crit (M_Sun)"],
-            marker=marker, c=color, alpha=0.5, s=markersize, edgecolors='none', zorder=5
-        )
-
-    # Display observational data
-    Budzynski14 = obs.Budzynski14()
-    Kravtsov18 = obs.Kravtsov18()
-    ax.plot(Budzynski14.M500_trials, Budzynski14.Mstar_trials_median / Budzynski14.M500_trials,
-            linestyle='-', linewidth=1.2, color=(0.8, 0.8, 0.8), zorder=0)
-    ax.fill_between(Budzynski14.M500_trials,
-                    Budzynski14.Mstar_trials_lower / Budzynski14.M500_trials,
-                    Budzynski14.Mstar_trials_upper / Budzynski14.M500_trials,
-                    color=(0.85, 0.85, 0.85), linewidth=0, edgecolor='none', zorder=0)
-    ax.scatter(Kravtsov18.M500, Kravtsov18.Mstar500 / Kravtsov18.M500, marker='*', s=12, color=(0.85, 0.85, 0.85),
-               edgecolors='none', zorder=0)
-
-    # Build legends
-    handles = [
-        Line2D([], [], marker='.', markeredgecolor='black', markerfacecolor='none', markeredgewidth=1,
-               linestyle='None', markersize=6, label='-8 Res'),
-        Line2D([], [], marker='^', markeredgecolor='black', markerfacecolor='none', markeredgewidth=1,
-               linestyle='None', markersize=3, label='+1 Res'),
-        Patch(facecolor='black', edgecolor='None', label='Random (Ref)'),
-        Patch(facecolor='orange', edgecolor='None', label='Minimum distance'),
-        Patch(facecolor='lime', edgecolor='None', label='Isotropic'),
-    ]
-    legend_sims = plt.legend(handles=handles, loc=2)
-    handles = [
-        Line2D([], [], color=(0.85, 0.85, 0.85), marker='.', markeredgecolor='none', linestyle='-', markersize=0,
-               label=Budzynski14.paper_name),
-        Line2D([], [], color=(0.85, 0.85, 0.85), marker='*', markeredgecolor='none', linestyle='None', markersize=4,
-               label=Kravtsov18.paper_name),
-        Line2D([], [], color='black', linestyle='--', markersize=0, label=f"Planck18 $f_{{bary}}=${fbary:.3f}"),
-    ]
-    del Budzynski14, Kravtsov18
-    legend_obs = plt.legend(handles=handles, loc=4)
-    ax.add_artist(legend_sims)
-    ax.add_artist(legend_obs)
-    ax.plot(ax.get_xlim(), [fbary for lim in ax.get_xlim()], '--', color='k')
-
-    ax.set_xlabel(r'$M_{500{\rm crit}}\ [{\rm M}_{\odot}]$')
-    ax.set_ylabel(r'$f_{{\rm star},500{\rm crit}}$')
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    fig.savefig(f'{zooms_register[0].output_directory}/f_500_star.png', dpi=300)
-    plt.show()
-    plt.close()
-
-    return
-
-
-def f_500_bary():
-    fig, ax = plt.subplots()
-
-    # The results of the multiprocessing Pool are returned in the same order as inputs
-    with Pool() as pool:
-        print(f"Analysis mapped onto {cpu_count():d} CPUs.")
-        results = pool.map(_process_single_halo, iter(zooms_register))
-
-        # Recast output into a Pandas dataframe for further manipulation
-        columns = [
-            'M_500crit (M_Sun)',
-            'M_star (< R_500crit) (M_Sun)',
-            'M_hot (< R_500crit) (M_Sun)',
-        ]
-        results = pd.DataFrame(list(results), columns=columns)
-        results.insert(0, 'Run name', pd.Series(name_list, dtype=str))
-        print(results)
-
-    # Display zoom data
-    for i in range(len(results)):
-
-        marker = ''
-        if '-8res' in results.loc[i, "Run name"]:
-            marker = '.'
-        elif '+1res' in results.loc[i, "Run name"]:
-            marker = '^'
-
-        color = ''
-        if 'Ref' in results.loc[i, "Run name"]:
-            color = 'black'
-        elif 'MinimumDistance' in results.loc[i, "Run name"]:
-            color = 'orange'
-        elif 'Isotropic' in results.loc[i, "Run name"]:
-            color = 'lime'
-
-        markersize = 14
-        if marker == '.':
-            markersize *= 1.5
-
-        ax.scatter(
-            results.loc[i, "M_500crit (M_Sun)"],
-            (results.loc[i, "M_star (< R_500crit) (M_Sun)"] + results.loc[i, "M_hot (< R_500crit) (M_Sun)"]) \
-            / results.loc[i, "M_500crit (M_Sun)"],
-            marker=marker, c=color, alpha=0.5, s=markersize, edgecolors='none', zorder=5
-        )
-
-    # Display observational data
-    # Budzynski14 = obs.Budzynski14()
-    # Kravtsov18 = obs.Kravtsov18()
-    # ax.plot(Budzynski14.M500_trials, Budzynski14.Mstar_trials_median / Budzynski14.M500_trials,
-    #         linestyle='-', color=(0.8, 0.8, 0.8), zorder=0)
-    # ax.fill_between(Budzynski14.M500_trials,
-    #                 Budzynski14.Mstar_trials_lower / Budzynski14.M500_trials,
-    #                 Budzynski14.Mstar_trials_upper / Budzynski14.M500_trials,
-    #                 linestyle='-', color=(0.85, 0.85, 0.85), edgecolor='none', zorder=0)
-    # ax.scatter(Kravtsov18.M500, Kravtsov18.Mstar500 / Kravtsov18.M500, marker='*', s=12, color=(0.85, 0.85, 0.85),
-    #            edgecolors='none', zorder=0)
-
-    # Build legends
-    handles = [
-        Line2D([], [], marker='.', markeredgecolor='black', markerfacecolor='none', markeredgewidth=1,
-               linestyle='None', markersize=6, label='-8 Res'),
-        Line2D([], [], marker='^', markeredgecolor='black', markerfacecolor='none', markeredgewidth=1,
-               linestyle='None', markersize=3, label='+1 Res'),
-        Patch(facecolor='black', edgecolor='None', label='Random (Ref)'),
-        Patch(facecolor='orange', edgecolor='None', label='Minimum distance'),
-        Patch(facecolor='lime', edgecolor='None', label='Isotropic'),
-    ]
-    legend_sims = plt.legend(handles=handles, loc=2)
-    # handles = [
-    #     Line2D([], [], color=(0.85, 0.85, 0.85), marker='.', markeredgecolor='none', linestyle='-', markersize=0,
-    #            label=Budzynski14.paper_name),
-    #     Line2D([], [], color=(0.85, 0.85, 0.85), marker='*', markeredgecolor='none', linestyle='None', markersize=4,
-    #            label=Kravtsov18.paper_name),
-    #     Line2D([], [], color='black', linestyle='--', markersize=0, label=f"Planck18 $f_{{bary}}=${fbary:.3f}"),
-    # ]
-    # del Budzynski14, Kravtsov18
-    # legend_obs = plt.legend(handles=handles, loc=4)
-    ax.add_artist(legend_sims)
-    # ax.add_artist(legend_obs)
-    ax.plot(ax.get_xlim(), [fbary for lim in ax.get_xlim()], '--', color='k')
-
-    ax.set_xlabel(r'$M_{500{\rm crit}}\ [{\rm M}_{\odot}]$')
-    ax.set_ylabel(r'$f_{{\rm bary},500{\rm crit}}$')
-    ax.set_xscale('log')
-    fig.savefig(f'{zooms_register[0].output_directory}/f_500_bary.png', dpi=300)
-    plt.show()
-    plt.close()
-
-    return
-
-
-m_500_star()
-f_500_star()
-f_500_bary()
+if __name__ == "__main__":
+    results = utils.process_catalogue(_process_single_halo, find_keyword='Ref')
+    m_500_star(results)
