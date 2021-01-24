610624e0078deacc7cf7ca7f9cadc4fc24a4d06a
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-24 15:00:10 +0100

    k scaling relations

--- scaling_relations/scaling_style.py
+++ scaling_relations/scaling_style.py
@@ -6,26 +6,26 @@ from matplotlib.cm import get_cmap
 
 name = "Set2"
 cmap = get_cmap(name)  # type: matplotlib.colors.ListedColormap
-colors = cmap.colors  # type: list
+colors = cmap.colors[::-1]  # type: list
 
 style_subgrid_model = pd.DataFrame(
     [
-        ['fixedAGNdT7.5_Nheat1_SNnobirth', colors[0], '.', 14, '-', 1,
+        ['fixedAGNdT7.5_Nheat1_SNnobirth', colors[0], '.', 23, '-', 1,
          Patch(facecolor=colors[0], edgecolor='None', label=r'fixed AGN $dT=10^{7.5}$ K')],
-        ['fixedAGNdT8_Nheat1_SNnobirth', colors[1], '.', 14, '-', 1,
+        ['fixedAGNdT8_Nheat1_SNnobirth', colors[1], '.', 23, '-', 1,
          Patch(facecolor=colors[1], edgecolor='None', label=r'fixed AGN $dT=10^8$ K')],
-        ['fixedAGNdT8.5_Nheat1_SNnobirth', colors[2], '.', 14, '-', 1,
+        ['fixedAGNdT8.5_Nheat1_SNnobirth', colors[2], '.', 23, '-', 1,
          Patch(facecolor=colors[2], edgecolor='None', label=r'fixed AGN $dT=10^{8.5}$ K')],
-        ['fixedAGNdT9_Nheat1_SNnobirth', colors[3], '.', 14, '-', 1,
+        ['fixedAGNdT9_Nheat1_SNnobirth', colors[3], '.', 23, '-', 1,
          Patch(facecolor=colors[3], edgecolor='None', label=r'fixed AGN $dT=10^9$ K')],
-        ['fixedAGNdT9.5_Nheat1_SNnobirth', colors[4], '.', 14, '-', 1,
+        ['fixedAGNdT9.5_Nheat1_SNnobirth', colors[4], '.', 23, '-', 1,
          Patch(facecolor=colors[4], edgecolor='None', label=r'fixed AGN $dT=10^{9.5}$ K')],
-        ['Ref', colors[5], '.', 14, '-', 1,
-         Patch(facecolor=colors[5], edgecolor='None', label=r'Random AGN fixed $dT$')],
-        ['MinimumDistance', colors[6], '.', 14, '-', 1,
-         Patch(facecolor=colors[6], edgecolor='None', label=r'Minimum distance AGN fixed $dT$')],
-        ['Isotropic', colors[7], '.', 14, '-', 1,
-         Patch(facecolor=colors[7], edgecolor='None', label=r'Isotropic AGN fixed $dT$')],
+        ['Ref', colors[5], '.', 23, '-', 1,
+         Patch(facecolor=colors[5], edgecolor='None', label=r'Random AGN variable $dT$')],
+        ['MinimumDistance', colors[6], '.', 23, '-', 1,
+         Patch(facecolor=colors[6], edgecolor='None', label=r'Minimum distance AGN variable $dT$')],
+        ['Isotropic', colors[7], '.', 23, '-', 1,
+         Patch(facecolor=colors[7], edgecolor='None', label=r'Isotropic AGN variable $dT$')],
     ],
     columns=['Run name keyword', 'Color', 'Marker style', 'Marker size', 'Line style', 'Line width', 'Legend handle']
 )
