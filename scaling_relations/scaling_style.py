b3fd35ffe20044d381cf809f25e4915b0ad37513
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-24 14:53:48 +0100

    k scaling relations

--- scaling_relations/scaling_style.py
+++ scaling_relations/scaling_style.py
@@ -32,6 +32,6 @@ style_subgrid_model = pd.DataFrame(
 
 
 def get_style_for_object(run_name: str):
-    for i in style_subgrid_model:
+    for i in len(style_subgrid_model):
         if run_name.endswith(style_subgrid_model.loc[i, 'Run name keyword']):
             return style_subgrid_model.loc[i]
