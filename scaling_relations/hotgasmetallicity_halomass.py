4247cffcfd1eb53b661db14ead3ef3645e6532b8
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-24 16:59:39 +0100

    k scaling relations

--- scaling_relations/hotgasmetallicity_halomass.py
+++ scaling_relations/hotgasmetallicity_halomass.py
@@ -70,13 +70,77 @@ def process_single_halo(
     index = np.where((deltaR > 0.15 * R500c) & (deltaR < R500c))[0]
     Thot500c_nocore = np.sum(mass_weighted_temperatures[index]) / np.sum(massGas[index])
 
+    # Convert temperatures to keV
+    Thot500c = (Thot500c * unyt.boltzmann_constant).to('keV')
+    Thot500c_nocore = (Thot500c_nocore * unyt.boltzmann_constant).to('keV')
+
     return M500c, Thot500c, Thot500c_nocore, Zhot500c, iron_fraction_500c
+
 
 @utils.set_scaling_relation_name(os.path.splitext(os.path.basename(__file__))[0])
 @utils.set_output_names([
     'M_500crit',
     'Thot500c',
-    'Thot500c_nocore'
+    'Thot500c_nocore',
+    'Zhot500c',
+    'iron_fraction_500c'
 ])
 def _process_single_halo(zoom: Zoom):
     return process_single_halo(zoom.snapshot_file, zoom.catalog_file)
+
+
+def m500_Fe500(results: pd.DataFrame):
+    fig, ax = plt.subplots()
+    legend_handles = []
+    for i in range(len(results)):
+
+        run_style = style.get_style_for_object(results.loc[i, "Run name"])
+        if run_style['Legend handle'] not in legend_handles:
+            legend_handles.append(run_style['Legend handle'])
+
+        ax.scatter(
+            results.loc[i, "M_500crit"],
+            results.loc[i, "iron_fraction_500c"],
+            marker=run_style['Marker style'],
+            c=run_style['Color'],
+            s=run_style['Marker size'],
+            alpha=1,
+            edgecolors='none',
+            zorder=5
+        )
+
+    # Build legends
+    legend_sims = plt.legend(handles=legend_handles, loc=2)
+    ax.add_artist(legend_sims)
+
+    # Display observational data
+    # Budzynski14 = obs.Budzynski14()
+    # Kravtsov18 = obs.Kravtsov18()
+    # ax.plot(Budzynski14.M500_trials, Budzynski14.Mstar_trials_median, linestyle='-', color=(0.8, 0.8, 0.8), zorder=0)
+    # ax.fill_between(Budzynski14.M500_trials, Budzynski14.Mstar_trials_lower, Budzynski14.Mstar_trials_upper,
+    #                 linewidth=0, color=(0.85, 0.85, 0.85), edgecolor='none', zorder=0)
+    # ax.scatter(Kravtsov18.M500, Kravtsov18.Mstar500, marker='*', s=12, color=(0.85, 0.85, 0.85),
+    #            linewidth=1.2, edgecolors='none', zorder=0)
+    #
+    # handles = [
+    #     Line2D([], [], color=(0.85, 0.85, 0.85), marker='.', markeredgecolor='none', linestyle='-', markersize=0,
+    #            label=Budzynski14.paper_name),
+    #     Line2D([], [], color=(0.85, 0.85, 0.85), marker='*', markeredgecolor='none', linestyle='None', markersize=4,
+    #            label=Kravtsov18.paper_name),
+    # ]
+    # del Budzynski14, Kravtsov18
+    # legend_obs = plt.legend(handles=handles, loc=4)
+    # ax.add_artist(legend_obs)
+
+    ax.set_xlabel(r'$M_{500{\rm crit}}\ [{\rm M}_{\odot}]$')
+    ax.set_ylabel(r'${\rm Fe}_{500{\rm crit}}\ [{\rm Z}_{\odot}]$')
+    ax.set_xscale('log')
+    ax.set_yscale('log')
+    fig.savefig(f'{zooms_register[0].output_directory}/m500_Fe500.png', dpi=300)
+    plt.show()
+    plt.close()
+
+
+if __name__ == "__main__":
+    results = utils.process_catalogue(_process_single_halo, find_keyword='Ref')
+    m500_Fe500(results)
