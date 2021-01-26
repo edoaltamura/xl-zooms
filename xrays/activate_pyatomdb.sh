ea40c34f4c3161e2fce993482b078df6c7c33fdb
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-01-26 22:16:59 +0100

    k scaling relations

---
+++ xrays/activate_pyatomdb.sh
@@ -0,0 +1,3 @@
+export ATOMDB=/cosma/home/dp004/dc-alta2/data7/xl-zooms/analysis/atomdb
+source /cosma/home/dp004/dc-alta2/data7/xl-zooms/analysis/xray-analysis/bin/activate
+echo "To deactivate this python venv, use the 'deactivate' command."
