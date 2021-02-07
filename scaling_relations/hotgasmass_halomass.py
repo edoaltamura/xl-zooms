8de3b9515687cb8a4d36ff1cc5f6f80e801248db
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-02-07 19:22:00 +0100

    k scaling relations

--- scaling_relations/hotgasmass_halomass.py
+++ scaling_relations/hotgasmass_halomass.py
@@ -173,7 +173,7 @@ def m_500_hotgas(results: pd.DataFrame):
                marker='s', s=5, alpha=1, facecolors='w', edgecolors='k', linewidth=0.4, zorder=0)
     handles.append(
         Line2D([], [], color='k', marker='s', markeredgecolor='none', linestyle='None', markersize=4,
-               label=obs.Barnes17().citation)
+               label=obs.Barnes17().citation + ' $z=0$')
     )
     del Barnes17
 
@@ -273,16 +273,15 @@ def f_500_hotgas(results: pd.DataFrame):
     )
     del Vikhlinin06
 
-    Barnes17 = obs.Barnes17()
-    ax.scatter(Barnes17.m_500true[Barnes17.ekin_ethrm < 0.1],
-               Barnes17.m_gas[Barnes17.ekin_ethrm < 0.1] / Barnes17.m_500spec[Barnes17.ekin_ethrm < 0.1],
+    Barnes17 = obs.Barnes17().hdf5.z000p000.true
+    relaxed = Barnes17.Ekin_500 / Barnes17.Ethm_500
+    ax.scatter(Barnes17.M500[relaxed < 0.1], Barnes17.Mgas_500[relaxed < 0.1] / Barnes17.M500[relaxed < 0.1],
                marker='s', s=6, alpha=1, color='k', edgecolors='none', zorder=0)
-    ax.scatter(Barnes17.m_500true[Barnes17.ekin_ethrm > 0.1],
-               Barnes17.m_gas[Barnes17.ekin_ethrm > 0.1] / Barnes17.m_500spec[Barnes17.ekin_ethrm > 0.1],
+    ax.scatter(Barnes17.M500[relaxed > 0.1], Barnes17.Mgas_500[relaxed > 0.1] / Barnes17.M500[relaxed > 0.1],
                marker='s', s=5, alpha=1, facecolors='w', edgecolors='k', linewidth=0.4, zorder=0)
     handles.append(
         Line2D([], [], color='k', marker='s', markeredgecolor='none', linestyle='None', markersize=4,
-               label=Barnes17.citation)
+               label=obs.Barnes17().citation + ' $z=0$')
     )
     del Barnes17
 
