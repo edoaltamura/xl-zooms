b297f6fd0eeeccdd08dba4c88d0c803b1498ce64
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-02-12 22:21:16 +0100

    k scaling relations

--- xray/apec_softband.py
+++ xray/apec_softband.py
@@ -180,8 +180,8 @@ def calc_spec(data):
     Fe_H = 10.0 ** (np.log10(Fe_H * (1.00794 / 55.845)) - AG_Fe)
 
     # Emission measure & Y parameter
-    EMM = (data.gas.densities.in_cgs() * (
-                (ne_nH / ((ne_nH + ni_nH) * mu * unyt.proton_mass_cgs)) ** 2.0)) * data.gas.masses.in_cgs() / ne_nH
+    EMM = (data.gas.densities * (
+                (ne_nH / ((ne_nH + ni_nH) * mu * unyt.proton_mass)) ** 2.0)) * data.gas.masses / ne_nH
 
     Ypar = (unyt.thompson_cross_section_cgs / (
                 511.0 * unyt.keV)) * unyt.boltzmann_constant_cgs * data.gas.temperatures * (
