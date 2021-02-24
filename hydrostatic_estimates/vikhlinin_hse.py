77ac9ea5d0334f0f28fefe60527a428db5681c62
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-02-24 10:56:17 +0100

    xray upload

--- hydrostatic_estimates/vikhlinin_hse.py
+++ hydrostatic_estimates/vikhlinin_hse.py
@@ -27,6 +27,7 @@ cosmology = obs.Observations().cosmo_model
 fbary = cosmology.Ob0 / cosmology.Om0  # Cosmic baryon fraction
 mean_molecular_weight = 0.5954
 true_data_nbins = 101
+fit_max_iterations = 4e6
 
 
 def histogram_unyt(
@@ -502,7 +503,7 @@ class HydrostaticEstimator:
                 (0.2 * self.R500c.v, np.inf),
                 (0.0, 5.0)
             ],
-            options={'maxiter': 40000, 'ftol': 1e-12}
+            options={'maxiter': fit_max_iterations, 'ftol': 1e-14}
         )
         return coeff_rho
 
@@ -515,14 +516,14 @@ class HydrostaticEstimator:
         bnds = ([0.0, 0.0, -3.0, 1.0, 0.0, 0.0, 1.0e-10, 0.0],
                 [np.inf, np.inf, 3.0, 5.0, 10.0, np.inf, 3.0, np.inf])
 
-        cf1 = least_squares(self.residuals_temperature, p0, bounds=bnds, args=(y, x), max_nfev=40000)
+        cf1 = least_squares(self.residuals_temperature, p0, bounds=bnds, args=(y, x), max_nfev=fit_max_iterations)
         mod1 = self.temperature_profile_model(
             x, cf1.x[0], cf1.x[1], cf1.x[2], cf1.x[3], cf1.x[4], cf1.x[5], cf1.x[6], cf1.x[7]
         )
         xis1 = np.sum((mod1 - y) ** 2.0 / y) / len(y)
 
         cf2 = minimize(self.residuals_temperature, p0, args=(y, x), method='Nelder-Mead',
-                       options={'maxiter': 40000, 'ftol': 1e-7})
+                       options={'maxiter': fit_max_iterations, 'ftol': 1e-10})
         mod2 = self.temperature_profile_model(
             x, cf2.x[0], cf2.x[1], cf2.x[2], cf2.x[3], cf2.x[4], cf2.x[5], cf2.x[6], cf2.x[7]
         )
