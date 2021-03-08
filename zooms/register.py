1d7f23644506bbcff296ac39cda26cb5a7593a12
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-03-08 19:44:00 +0100

    backend update

--- zooms/register.py
+++ zooms/register.py
@@ -72,36 +72,33 @@ def dump_memory_usage() -> None:
 
 
 def tail(f, window=20):
-    """Returns the last `window` lines of file `f` as a list.
+    """
+    Returns the last `window` lines of file `f` as a list.
+    f - a byte file-like object
     """
     if window == 0:
         return []
-
     BUFSIZ = 1024
     f.seek(0, 2)
-    remaining_bytes = f.tell()
+    bytes = f.tell()
     size = window + 1
     block = -1
     data = []
-
-    while size > 0 and remaining_bytes > 0:
-        if remaining_bytes - BUFSIZ > 0:
+    while size > 0 and bytes > 0:
+        if bytes - BUFSIZ > 0:
             # Seek back one whole BUFSIZ
             f.seek(block * BUFSIZ, 2)
             # read BUFFER
-            bunch = f.read(BUFSIZ)
+            data.insert(0, f.read(BUFSIZ))
         else:
-            # file too small, start from beginning
-            f.seek(0, 0)
+            # file too small, start from begining
+            f.seek(0,0)
             # only read what was not read
-            bunch = f.read(remaining_bytes)
-
-        bunch = bunch.decode('utf-8')
-        data.insert(0, bunch)
-        size -= bunch.count('\n')
-        remaining_bytes -= BUFSIZ
+            data.insert(0, f.read(bytes))
+        linesFound = data[0].count('\n')
+        size -= linesFound
+        bytes -= BUFSIZ
         block -= 1
-
     return ''.join(data).splitlines()[-window:]
 
 
