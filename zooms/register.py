1504a8082c782fed3986bebe66509c7275a59644
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-03-08 19:40:14 +0100

    backend update

--- zooms/register.py
+++ zooms/register.py
@@ -74,25 +74,29 @@ def dump_memory_usage() -> None:
 def tail(f, lines=2):
     total_lines_wanted = lines
 
-    block_size = 1024
+    BLOCK_SIZE = 1024
     f.seek(0, 2)
     block_end_byte = f.tell()
     lines_to_go = total_lines_wanted
     block_number = -1
-    blocks = []
+    blocks = []  # blocks of size BLOCK_SIZE, in reverse order starting
+    # from the end of the file
     while lines_to_go > 0 and block_end_byte > 0:
-        if block_end_byte - block_size > 0:
-            f.seek(block_number * block_size, 2)
-            blocks.append(f.read(block_size))
+        if (block_end_byte - BLOCK_SIZE > 0):
+            # read the last block we haven't yet read
+            f.seek(block_number * BLOCK_SIZE, 2)
+            blocks.append(f.read(BLOCK_SIZE))
         else:
+            # file too small, start from begining
             f.seek(0, 0)
+            # only read what was not read
             blocks.append(f.read(block_end_byte))
-        lines_found = blocks[-1].count(b'\n')
+        lines_found = blocks[-1].count('\n')
         lines_to_go -= lines_found
-        block_end_byte -= block_size
+        block_end_byte -= BLOCK_SIZE
         block_number -= 1
-    all_read_text = b''.join(reversed(blocks))
-    return b'\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
+    all_read_text = ''.join(reversed(blocks))
+    return '\n'.join(all_read_text.splitlines()[-total_lines_wanted:])
 
 
 class EXLZooms:
