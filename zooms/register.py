5ecce6358e332db91911116f4a32f0436f3805cb
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-03-08 23:36:56 +0100

    backend update

--- zooms/register.py
+++ zooms/register.py
@@ -334,11 +334,9 @@ class EXLZooms:
         )
 
         if stdout:
-            print(f"\n{' Detailed search on incomplete runs ':-^40s}")
             print((
-                f"{'':-^40s}\n"
+                f"\n{' Detailed search on incomplete runs ':-^40s}\n"
                 f"{' Legend ':-^40s}\n"
-                f"{'':-^40s}\n"
                 "`!` = Incomplete \n`SW` = SWIFT \n`VR` = Velociraptor \n"
                 "`z = 127.` = Simulation not started \n"
                 "`xxx` = Not on SLURM queue \n`que` = Pending in SLURM queue \n"
