69561a5118e843ad1bbea9ef47782074e963e8ad
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-03-10 19:37:26 +0100

    backend update

--- scaling_relations/relaxation.py
+++ scaling_relations/relaxation.py
@@ -208,5 +208,5 @@ def relaxation(results: pd.DataFrame):
 
 
 if __name__ == "__main__":
-    results = utils.process_catalogue(_process_single_halo, find_keyword=args.keywords, no_multithreading=True)
+    results = utils.process_catalogue(_process_single_halo, find_keyword=args.keywords)
     relaxation(results)
