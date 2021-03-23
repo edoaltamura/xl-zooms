4ff540d58e1b60f55ed8bb1afdd1f2b16e38490a
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-03-23 01:07:48 +0100

    backend update

--- profiles/radial_profiles_points.py
+++ profiles/radial_profiles_points.py
@@ -182,7 +182,7 @@ def plot_radial_profiles_median(object_database: pd.DataFrame, highmass_only: bo
     ydat1 = field[ind][hhsub < thresh]
     hh[hh < thresh] = np.nan  # fill the areas with low density by NaNs
 
-    im = ax.imshow(hh.T, cmap='copper', interpolation='none', origin='lower')
+    im = ax.imshow(hh.T, cmap='copper', interpolation='none', origin='lower', extent=[0.01, 2, 10, 2000])
     fig.colorbar(im)
     ax.plot(xdat1, ydat1, marker=',', lw=0, linestyle="", c='darkblue', alpha=0.9)
 
