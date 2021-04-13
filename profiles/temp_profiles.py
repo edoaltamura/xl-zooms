3e00d6c4bdb6701e6b48b76c254f8c1f887501a5
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-13 16:19:09 +0100

    backend update

--- profiles/temp_profiles.py
+++ profiles/temp_profiles.py
@@ -1,7 +1,75 @@
-from radial_profiles_points import profile_3d_single_halo
 from os.path import isfile
 from matplotlib import pyplot as plt
 from matplotlib.cm import get_cmap
+import unyt
+import numpy as np
+import swiftsimio as sw
+import velociraptor as vr
+
+# Constants
+mean_molecular_weight = 0.59
+mean_atomic_weight_per_free_electron = 1.14
+
+
+def profile_3d_single_halo(
+        path_to_snap: str,
+        path_to_catalogue: str,
+) -> tuple:
+    # Read in halo properties
+    vr_catalogue_handle = vr.load(path_to_catalogue)
+    a = vr_catalogue_handle.a
+    M500 = vr_catalogue_handle.spherical_overdensities.mass_500_rhocrit[0].to('Msun')
+    R500 = vr_catalogue_handle.spherical_overdensities.r_500_rhocrit[0].to('Mpc')
+    XPotMin = vr_catalogue_handle.positions.xcminpot[0].to('Mpc')
+    YPotMin = vr_catalogue_handle.positions.ycminpot[0].to('Mpc')
+    ZPotMin = vr_catalogue_handle.positions.zcminpot[0].to('Mpc')
+
+    # Apply spatial mask to particles. SWIFTsimIO needs comoving coordinates
+    # to filter particle coordinates, while VR outputs are in physical units.
+    # Convert the region bounds to comoving, but keep the CoP and Rcrit in
+    # physical units for later use.
+    mask = sw.mask(path_to_snap, spatial_only=True)
+    region = [
+        [(XPotMin - R500) * a, (XPotMin + R500) * a],
+        [(YPotMin - R500) * a, (YPotMin + R500) * a],
+        [(ZPotMin - R500) * a, (ZPotMin + R500) * a]
+    ]
+    mask.constrain_spatial(region)
+    data = sw.load(path_to_snap, mask=mask)
+
+    # Convert datasets to physical quantities
+    # R500c is already in physical units
+    data.gas.coordinates.convert_to_physical()
+    data.gas.masses.convert_to_physical()
+    data.gas.temperatures.convert_to_physical()
+    data.gas.densities.convert_to_physical()
+
+    # Select hot gas within sphere
+    tempGas = data.gas.temperatures
+    deltaX = data.gas.coordinates[:, 0] - XPotMin
+    deltaY = data.gas.coordinates[:, 1] - YPotMin
+    deltaZ = data.gas.coordinates[:, 2] - ZPotMin
+    radial_distance = np.sqrt(deltaX ** 2 + deltaY ** 2 + deltaZ ** 2) / R500
+    index = np.where((radial_distance < 2) & (tempGas > 1e5))[0]
+    del tempGas, deltaX, deltaY, deltaZ
+
+    # Calculate particle mass and rho_crit
+    rho_crit = unyt.unyt_quantity(
+        data.metadata.cosmology.critical_density(data.metadata.z).value,
+        'g/cm**3'
+    )
+
+    mass_weighted_temperatures = (data.gas.temperatures * unyt.boltzmann_constant).to('keV')
+    number_densities = (data.gas.densities.to('g/cm**3') / (unyt.mp * mean_molecular_weight)).to('cm**-3')
+    field_value = mass_weighted_temperatures / number_densities ** (2 / 3)
+
+    field_label = r'$K$ [keV cm$^2$]'
+    radial_distance = radial_distance[index]
+    field_value = field_value[index]
+    field_masses = data.gas.temperatures[index]
+
+    return radial_distance, field_value, field_masses, field_label, M500, R500
+
 
 cwd = '/cosma/home/dp004/dc-alta2/snap7/xl-zooms/hydro/'
 
@@ -29,8 +97,8 @@ paths = {
 }
 
 fig, ax = plt.subplots()
-name = "Set2"
-cmap = get_cmap(name)  # type: matplotlib.colors.ListedColormap
+name = "coolwarm"
+cmap = get_cmap(name)
 
 fig, ax = plt.subplots()
 
@@ -50,4 +118,3 @@ for alpha_key in paths:
     ax.plot(radial_distance[::20], field_value[::20], marker=',', lw=0, linestyle="", c=cmap(alpha_key), alpha=0.1)
 
 plt.show()
-
