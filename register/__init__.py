2811f8e92bccd8f47ff0f4b5da28d984ba269976
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-14 23:03:40 +0100

    Fix relative imports

--- register/__init__.py
+++ register/__init__.py
@@ -1,6 +1,9 @@
 from .auto_parser import (
-    args, parser, mpl_backend
+    args,
+    parser,
+    mpl_backend
 )
+
 from .static_parameters import *
 
 from .register import (
@@ -12,4 +15,9 @@ from .register import (
     completed_runs,
     zooms_register,
     name_list,
+)
+
+from .intermediate_io import (
+    SingleObjPickler,
+    MultiObjPickler
 )
