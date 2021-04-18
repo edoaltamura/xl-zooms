50a03ab2f4d39f817b4bd6881a8d8c0087e351ce
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-18 19:22:45 +0100

    Refractor testing and main locations

--- testing/test_literature.py
+++ testing/test_literature.py
@@ -1,24 +1,14 @@
-import unittest
-
 from literature import *
 
-
-class TestLiterature(unittest.TestCase):
-    def run_test(self):
-        Barnes2017()
-        Bohringer2007()
-        Budzynski2014()
-        Eckert2016()
-        Gonzalez2013()
-        Kravtsov2018()
-        Lin2012()
-        Lovisari2015()
-        PlanckSZ2015()
-        Pratt2010()
-        Sun2009()
-        Vikhlinin2006()
-        self.assertEqual(0, 0)
-
-
-if __name__ == '__main__':
-    unittest.main()
+Barnes2017()
+Bohringer2007()
+Budzynski2014()
+Eckert2016()
+Gonzalez2013()
+Kravtsov2018()
+Lin2012()
+Lovisari2015()
+PlanckSZ2015()
+Pratt2010()
+Sun2009()
+Vikhlinin2006()
--- testing/test_register.py
+++ testing/test_register.py
@@ -1,4 +1,3 @@
-import unittest
 import contextlib
 from tqdm import tqdm
 
@@ -8,31 +7,23 @@ from register import (
     zooms_register,
 )
 
+incomplete_runs = calibration_zooms.get_incomplete_run_directories()
+for _ in tqdm(incomplete_runs, desc='[Incomplete runs]'):
+    pass
+print('Done.')
 
-class TestRegister(unittest.TestCase):
-    def run_test(self):
-        incomplete_runs = calibration_zooms.get_incomplete_run_directories()
-        for _ in tqdm(incomplete_runs, desc='[Incomplete runs]'):
-            pass
-        print('Done.')
+print(f"\n{' Zoom register ':-^40s}")
+for _ in tqdm(completed_runs, desc='[Complete runs]'):
+    pass
+print('Done.')
 
-        print(f"\n{' Zoom register ':-^40s}")
-        for _ in tqdm(completed_runs, desc='[Complete runs]'):
-            pass
-        print('Done.')
+print(f"\n{' Test: redshift data (z = 0) ':-^40s}")
+print(zooms_register[0].get_redshift())
 
-        print(f"\n{' Test: redshift data (z = 0) ':-^40s}")
-        print(zooms_register[0].get_redshift())
+print(f"\n{' Test: redshift data (z = 0.1) ':-^40s}")
+print(zooms_register[0].get_redshift(-3))
 
-        print(f"\n{' Test: redshift data (z = 0.1) ':-^40s}")
-        print(zooms_register[0].get_redshift(-3))
-
-        print("Advanced search (incl SLURM) testing byt not printing...")
-        with contextlib.redirect_stdout(None):
-            calibration_zooms.analyse_incomplete_runs()
-        print('Done.')
-        self.assertEqual(0, 0)
-
-
-if __name__ == '__main__':
-    unittest.main()
+print("Advanced search (incl SLURM) testing byt not printing...")
+with contextlib.redirect_stdout(None):
+    calibration_zooms.analyse_incomplete_runs()
+print('Done.')
