6411b78be5698dbe707640d41b591479b697ced0
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-18 18:43:31 +0100

    Refractor testing and main locations

--- testing/test_literature.py
+++ testing/test_literature.py
@@ -1,16 +1,23 @@
+import unittest
 
 from literature import *
 
+
+class TestSum(unittest.TestCase):
+    def run_test(self):
+        Barnes2017()
+        Bohringer2007()
+        Budzynski2014()
+        Eckert2016()
+        Gonzalez2013()
+        Kravtsov2018()
+        Lin2012()
+        Lovisari2015()
+        PlanckSZ2015()
+        Pratt2010()
+        Sun2009()
+        Vikhlinin2006()
+
+
 if __name__ == '__main__':
-    Barnes2017()
-    Bohringer2007()
-    Budzynski2014()
-    Eckert2016()
-    Gonzalez2013()
-    Kravtsov2018()
-    Lin2012()
-    Lovisari2015()
-    PlanckSZ2015()
-    Pratt2010()
-    Sun2009()
-    Vikhlinin2006()
+    unittest.main()
