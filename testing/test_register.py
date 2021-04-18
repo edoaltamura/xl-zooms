1cebf01a5828abb9bb81adddc5d453e5f196b92f
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-18 21:26:01 +0100

    Refractor testing and main locations

--- testing/test_register.py
+++ testing/test_register.py
@@ -12,12 +12,12 @@ from register import (
 )
 
 incomplete_runs = calibration_zooms.get_incomplete_run_directories()
-for _ in tqdm(incomplete_runs, desc='[Incomplete runs]'):
+for _ in tqdm(incomplete_runs, desc='Incomplete runs'):
     pass
 print('Done.')
 
 print(f"\n{' Zoom register ':-^40s}")
-for _ in tqdm(completed_runs, desc='[Complete runs]'):
+for _ in tqdm(completed_runs, desc='Complete runs'):
     pass
 print('Done.')
 
@@ -27,7 +27,7 @@ print(zooms_register[0].get_redshift())
 print(f"\n{' Test: redshift data (z = 0.1) ':-^40s}")
 print(zooms_register[0].get_redshift(-3))
 
-print("Advanced search (incl SLURM) testing byt not printing...")
+print("\nAdvanced search (incl SLURM) testing byt not printing...")
 with contextlib.redirect_stdout(None):
     calibration_zooms.analyse_incomplete_runs()
 print('Done.')
