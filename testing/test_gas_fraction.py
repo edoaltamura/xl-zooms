3c72134686c357bbe6a7f26d56c0c35961221e55
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-21 14:10:40 +0100

    Refractor

--- testing/test_gas_fraction.py
+++ testing/test_gas_fraction.py
@@ -3,13 +3,7 @@ import sys
 sys.path.append("..")
 
 from scaling_relations.gas_fractions import GasFractions
-
-cwd = '/cosma/home/dp004/dc-alta2/snap7/xl-zooms/hydro/'
-runname = 'L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1'
-cat, snap = (
-        cwd + 'vr_partial_outputs/alpha1p0.properties',
-        cwd + runname + '_alpha1p0/snapshots/' + runname + '_SNnobirth_2252.hdf5'
-    )
+from .test_files import cat, snap
 
 gf = GasFractions()
 print(
