f3bbfe7e6023e48107f36a58853fb4b2f925ef69
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-23 15:10:28 +0100

    Update tqdm

--- main/entropy_mass_6panels.py
+++ main/entropy_mass_6panels.py
@@ -101,7 +101,8 @@ for model in models:
 
 handles.append(Line2D([], [], color='k', marker='*', markeredgecolor='none', linestyle='None', markersize=4,
                       label=sun2009.citation))
-plt.legend(handles=handles, frameon=True, facecolor='w', edgecolor='k', bbox_to_anchor=(0., 1.02, 1., .102),
+l = plt.legend(handles=handles, frameon=True, facecolor='w', edgecolor='k', bbox_to_anchor=(0., 1.02, 1., .102),
            loc='lower left',
            ncol=2, mode="expand", borderaxespad=0.)
+axes[0, 0].add_artist(l)
 plt.show()
