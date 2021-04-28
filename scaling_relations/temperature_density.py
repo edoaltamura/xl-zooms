c2b276f96dda4a0433308669c86dcac7291822c3
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-28 19:55:39 +0100

    Central BH mapp

--- scaling_relations/temperature_density.py
+++ scaling_relations/temperature_density.py
@@ -121,18 +121,20 @@ class TemperatureDensity(HaloProperty):
             temperature = sw_data.gas.temperatures.to('K').value[index]
         elif agn_time == 'before':
             number_density = (sw_data.gas.densities_before_last_agnevent / mh).to('cm**-3').value[index]
-            number_density[number_density < 0] = np.nan
+            number_density[number_density <= 0] = np.nan
             A = sw_data.gas.entropies_before_last_agnevent * sw_data.gas.masses
             rho = sw_data.gas.densities_before_last_agnevent
             temperature = mean_molecular_weight * (gamma - 1) * (A * rho ** (5 / 3 - 1)) / (gamma - 1) * mh / boltzmann_constant
             temperature = temperature.to('K').value[index]
+            temperature[temperature <= 0] = np.nan
         elif agn_time == 'after':
             number_density = (sw_data.gas.densities_at_last_agnevent / mh).to('cm**-3').value[index]
-            number_density[number_density < 0] = np.nan
+            number_density[number_density <= 0] = np.nan
             A = sw_data.gas.entropies_at_last_agnevent * sw_data.gas.masses
             rho = sw_data.gas.densities_at_last_agnevent
             temperature = mean_molecular_weight * (gamma - 1) * (A * rho ** (5 / 3 - 1)) / (gamma - 1) * mh / boltzmann_constant
             temperature = temperature.to('K').value[index]
+            temperature[temperature <= 0] = np.nan
 
         agn_flag = sw_data.gas.heated_by_agnfeedback[index]
         snii_flag = sw_data.gas.heated_by_sniifeedback[index]
