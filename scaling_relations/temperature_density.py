f6f85b47d61175ad60375110de72a8b794b4cbaa
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-29 11:54:13 +0100

    Central BH mapp

--- scaling_relations/temperature_density.py
+++ scaling_relations/temperature_density.py
@@ -195,7 +195,7 @@ class TemperatureDensity(HaloProperty):
 
             n_adiabats = np.array([1e-4, 1]) * nH_500.units
             T_adiabats = 10 ** (np.log10(K500 / boltzmann_constant) + (2 / 3) * np.log10(n_adiabats))
-            ax.plot(n_adiabats, T_adiabats, marker=None, linewidth=1, color='r')
+            # ax.plot(n_adiabats, T_adiabats, marker=None, linewidth=1, color='r')
 
             draw_adiabats(ax, density_bins, temperature_bins)
 
