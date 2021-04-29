d7abdf621eb080eadb2def7a9430a4c3b1a5214e
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-29 12:03:12 +0100

    Central BH mapp

--- scaling_relations/temperature_density.py
+++ scaling_relations/temperature_density.py
@@ -193,9 +193,9 @@ class TemperatureDensity(HaloProperty):
             K500 = (T500 * K * boltzmann_constant / (3 * m500 * Cosmology().fb / (4 * np.pi * r500 ** 3 * mp)) ** (
                         2 / 3)).to('keV*cm**2')
 
-            nlog_adiabats = np.array([-4, 0])
+            nlog_adiabats = np.array([-4., 0.])
             Tlog_adiabats = np.log10(K500 / boltzmann_constant) + (2 / 3) * nlog_adiabats
-            ax.plot(10 ** nlog_adiabats, 10 ** Tlog_adiabats, linewidth=1, color='r')
+            ax.plot(10. ** nlog_adiabats, 10. ** Tlog_adiabats, linewidth=1, color='r')
 
             draw_adiabats(ax, density_bins, temperature_bins)
 
