0988c7d66521c3c3fd41ec3ca07e9680d8517cf2
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-30 18:29:38 +0100

    Central BH mapp

--- testing/test_entropy_heat_vs_snap.py
+++ testing/test_entropy_heat_vs_snap.py
@@ -10,23 +10,7 @@ gf = EntropyComparison()
 gf.process_single_halo(
         path_to_snap=snap,
         path_to_catalogue=cat,
-        agn_time='before',
-        z_agn_start=1,
-        z_agn_end=0
-    )
-
-gf.process_single_halo(
-        path_to_snap=snap,
-        path_to_catalogue=cat,
-        agn_time='before',
-        z_agn_start=3,
-        z_agn_end=1
-    )
-
-gf.process_single_halo(
-        path_to_snap=snap,
-        path_to_catalogue=cat,
-        agn_time='before',
+        agn_time='after',
         z_agn_start=18,
-        z_agn_end=3
+        z_agn_end=0
     )
