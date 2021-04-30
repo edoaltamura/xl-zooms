f147de8e50a4dd297e51baf4859acea552a0cdcf
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-04-30 17:02:07 +0100

    Central BH mapp

--- testing/test_temperature_density.py
+++ testing/test_temperature_density.py
@@ -7,50 +7,50 @@ from test_files import cat, snap
 
 gf = TemperatureDensity()
 
-gf.process_single_halo(
-        path_to_snap=snap,
-        path_to_catalogue=cat,
-        agn_time=None,
-        z_agn_start=18,
-        z_agn_end=0
-    )
-
-gf.process_single_halo(
-        path_to_snap=snap,
-        path_to_catalogue=cat,
-        agn_time='before',
-        z_agn_start=18,
-        z_agn_end=0
-    )
-
-gf.process_single_halo(
-        path_to_snap=snap,
-        path_to_catalogue=cat,
-        agn_time='after',
-        z_agn_start=18,
-        z_agn_end=0
-    )
-
 # gf.process_single_halo(
 #         path_to_snap=snap,
 #         path_to_catalogue=cat,
 #         agn_time=None,
-#         z_agn_start=1,
+#         z_agn_start=18,
 #         z_agn_end=0
 #     )
 #
 # gf.process_single_halo(
 #         path_to_snap=snap,
 #         path_to_catalogue=cat,
-#         agn_time=None,
-#         z_agn_start=3,
-#         z_agn_end=1
+#         agn_time='before',
+#         z_agn_start=18,
+#         z_agn_end=0
 #     )
 #
 # gf.process_single_halo(
 #         path_to_snap=snap,
 #         path_to_catalogue=cat,
-#         agn_time=None,
+#         agn_time='after',
 #         z_agn_start=18,
-#         z_agn_end=3
+#         z_agn_end=0
 #     )
+
+gf.process_single_halo(
+        path_to_snap=snap,
+        path_to_catalogue=cat,
+        agn_time=None,
+        z_agn_start=1,
+        z_agn_end=0
+    )
+
+gf.process_single_halo(
+        path_to_snap=snap,
+        path_to_catalogue=cat,
+        agn_time=None,
+        z_agn_start=3,
+        z_agn_end=1
+    )
+
+gf.process_single_halo(
+        path_to_snap=snap,
+        path_to_catalogue=cat,
+        agn_time=None,
+        z_agn_start=18,
+        z_agn_end=3
+    )
