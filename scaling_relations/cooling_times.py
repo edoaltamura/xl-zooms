6160d19bbfd76abfdd456d831d727918f5a5c550
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-05-05 16:59:06 +0100

    Cooling contours

--- scaling_relations/cooling_times.py
+++ scaling_relations/cooling_times.py
@@ -405,24 +405,24 @@ class CoolingTimes(HaloProperty):
 
         print("Number of particles being plotted", len(x))
 
-        # Set the limits of the figure.
-        assert (x > 0).all(), f"Found negative value(s) in x: {x[x <= 0.]}"
-        assert (y > 0).all(), f"Found negative value(s) in y: {y[y <= 0.]}"
-        assert (w > 0).all(), f"Found negative value(s) in w: {w[w <= 0.]}"
-
-        # density_bounds = [1e-6, 1e4]  # in nh/cm^3
-        # temperature_bounds = [1e3, 1e10]  # in K
-        density_bounds = [1e-6, 1]  # in nh/cm^3
-        temperature_bounds = [1e6, 1e10]  # in K
-        bins = 256
-
-        # Make the norm object to define the image stretch
-        density_bins = np.logspace(
-            np.log10(density_bounds[0]), np.log10(density_bounds[1]), bins
-        )
-        temperature_bins = np.logspace(
-            np.log10(temperature_bounds[0]), np.log10(temperature_bounds[1]), bins
-        )
+        # # Set the limits of the figure.
+        # assert (x > 0).all(), f"Found negative value(s) in x: {x[x <= 0.]}"
+        # assert (y > 0).all(), f"Found negative value(s) in y: {y[y <= 0.]}"
+        # assert (w > 0).all(), f"Found negative value(s) in w: {w[w <= 0.]}"
+        #
+        # # density_bounds = [1e-6, 1e4]  # in nh/cm^3
+        # # temperature_bounds = [1e3, 1e10]  # in K
+        # density_bounds = [1e-6, 1]  # in nh/cm^3
+        # temperature_bounds = [1e6, 1e10]  # in K
+        # bins = 256
+        #
+        # # Make the norm object to define the image stretch
+        # density_bins = np.logspace(
+        #     np.log10(density_bounds[0]), np.log10(density_bounds[1]), bins
+        # )
+        # temperature_bins = np.logspace(
+        #     np.log10(temperature_bounds[0]), np.log10(temperature_bounds[1]), bins
+        # )
 
         # fig = plt.figure(figsize=(5, 5))
         # gs = fig.add_gridspec(1, 1, hspace=0.1, wspace=0.2)
