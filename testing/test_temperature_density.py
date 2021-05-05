2ec55e069003b38bd4ea6bc4540fdb751f15cf0d
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-05-05 11:56:21 +0100

    Cooling contours

--- testing/test_temperature_density.py
+++ testing/test_temperature_density.py
@@ -7,13 +7,13 @@ from test_files import cat, snap
 
 gf = TemperatureDensity()
 
-# gf.process_single_halo(
-#         path_to_snap=snap,
-#         path_to_catalogue=cat,
-#         agn_time=None,
-#         z_agn_start=18,
-#         z_agn_end=0
-#     )
+gf.process_single_halo(
+        path_to_snap=snap,
+        path_to_catalogue=cat,
+        agn_time=None,
+        z_agn_start=18,
+        z_agn_end=0
+    )
 #
 # gf.process_single_halo(
 #         path_to_snap=snap,
@@ -31,26 +31,26 @@ gf = TemperatureDensity()
 #         z_agn_end=0
 #     )
 
-gf.process_single_halo(
-        path_to_snap=snap,
-        path_to_catalogue=cat,
-        agn_time='before',
-        z_agn_start=1,
-        z_agn_end=0
-    )
-
-gf.process_single_halo(
-        path_to_snap=snap,
-        path_to_catalogue=cat,
-        agn_time='before',
-        z_agn_start=3,
-        z_agn_end=1
-    )
-
-gf.process_single_halo(
-        path_to_snap=snap,
-        path_to_catalogue=cat,
-        agn_time='before',
-        z_agn_start=18,
-        z_agn_end=3
-    )
+# gf.process_single_halo(
+#         path_to_snap=snap,
+#         path_to_catalogue=cat,
+#         agn_time='before',
+#         z_agn_start=1,
+#         z_agn_end=0
+#     )
+#
+# gf.process_single_halo(
+#         path_to_snap=snap,
+#         path_to_catalogue=cat,
+#         agn_time='before',
+#         z_agn_start=3,
+#         z_agn_end=1
+#     )
+#
+# gf.process_single_halo(
+#         path_to_snap=snap,
+#         path_to_catalogue=cat,
+#         agn_time='before',
+#         z_agn_start=18,
+#         z_agn_end=3
+#     )
