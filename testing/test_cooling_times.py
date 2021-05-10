65a720468e7997e7c74e82ae4c84e37a8e5bf165
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-05-10 17:46:03 +0100

    Cooling contours

--- testing/test_cooling_times.py
+++ testing/test_cooling_times.py
@@ -58,19 +58,19 @@ gf = CoolingTimes()
 dir = '/cosma/home/dp004/dc-alta2/data6/xl-zooms/hydro/'
 
 snaps = [dir + i for i in [
-    "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT7.5_Nheat1_SNnobirth/snapshots/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT7.5_Nheat1_SNnobirth_0036.hdf5",
-    "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth/snapshots/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth_0036.hdf5",
-    "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8_Nheat1_SNnobirth/snapshots/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8_Nheat1_SNnobirth_0036.hdf5",
-    "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9.5_Nheat1_SNnobirth/snapshots/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9.5_Nheat1_SNnobirth_0036.hdf5",
+    # "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT7.5_Nheat1_SNnobirth/snapshots/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT7.5_Nheat1_SNnobirth_0036.hdf5",
+    # "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth/snapshots/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth_0036.hdf5",
+    # "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8_Nheat1_SNnobirth/snapshots/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8_Nheat1_SNnobirth_0036.hdf5",
+    "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9.5_Nheat1_SNnobirth/snapshots/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9.5_Nheat1_SNnobirth_2749.hdf5",
     "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9_Nheat1_SNnobirth/snapshots/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9_Nheat1_SNnobirth_0036.hdf5",
 ]
          ]
 
 cats = [dir + i for i in [
-    "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT7.5_Nheat1_SNnobirth/stf/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT7.5_Nheat1_SNnobirth_0036/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT7.5_Nheat1_SNnobirth_0036.properties",
-    "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth/stf/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth_0036/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth_0036.properties",
-    "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8_Nheat1_SNnobirth/stf/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8_Nheat1_SNnobirth_0036/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8_Nheat1_SNnobirth_0036.properties",
-    "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9.5_Nheat1_SNnobirth/stf/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9.5_Nheat1_SNnobirth_0036/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9.5_Nheat1_SNnobirth_0036.properties",
+    # "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT7.5_Nheat1_SNnobirth/stf/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT7.5_Nheat1_SNnobirth_0036/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT7.5_Nheat1_SNnobirth_0036.properties",
+    # "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth/stf/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth_0036/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth_0036.properties",
+    # "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8_Nheat1_SNnobirth/stf/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8_Nheat1_SNnobirth_0036/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8_Nheat1_SNnobirth_0036.properties",
+    "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9.5_Nheat1_SNnobirth/stf/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9.5_Nheat1_SNnobirth_2749/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9.5_Nheat1_SNnobirth_2749.properties",
     "L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9_Nheat1_SNnobirth/stf/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9_Nheat1_SNnobirth_0036/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT9_Nheat1_SNnobirth_0036.properties",
 ]
          ]
