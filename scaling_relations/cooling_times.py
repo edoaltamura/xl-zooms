0aacbb7426e3b0efdbb12e317ec5ca624ec09a5f
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-05-13 14:53:32 +0100

    Cooling contours

--- scaling_relations/cooling_times.py
+++ scaling_relations/cooling_times.py
@@ -742,8 +742,8 @@ class CoolingTimes(HaloProperty):
         mass_weighted_temp_map = project_gas(project='mwtemps', **map_kwargs)
         mass_map = project_gas(project='masses', **map_kwargs)
 
-        mass_weighted_temp_map = np.ma.array(mass_weighted_temp_map, mask=(mass_weighted_temp_map <= 0.))
-        mass_map = np.ma.array(mass_map, mask=(mass_map <= 0.))
+        mass_weighted_temp_map = np.ma.array(mass_weighted_temp_map, mask=(mass_weighted_temp_map <= 0.), fill_value=np.nan, copy=True, dtype=np.float64)
+        mass_map = np.ma.array(mass_map, mask=(mass_map <= 0.), fill_value=np.nan, copy=True, dtype=np.float64)
         gas_temp = mass_weighted_temp_map / mass_map
         gas_temp = np.ma.array(gas_temp, mask=np.isnan(gas_temp), fill_value=np.nan, copy=True, dtype=np.float64)
 
