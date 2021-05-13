0457641709f40a878e37dae610432b3ad0077644
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-05-13 19:01:46 +0100

    Cooling contours

--- main/cooling_times_movie.py
+++ main/cooling_times_movie.py
@@ -1,9 +1,4 @@
-# from mpi4py import MPI
-#
-# comm = MPI.COMM_WORLD
-# num_processes = comm.size
-# rank = comm.rank
-import gc
+from mpi4py import MPI
 import sys
 
 sys.path.append("..")
@@ -20,26 +15,32 @@ parser.add_argument(
 
 args = parser.parse_args()
 
+comm = MPI.COMM_WORLD
+num_processes = comm.size
+rank = comm.rank
+
 name = 'L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth'
 dir = '/cosma/home/dp004/dc-alta2/snap7/xl-zooms/hydro/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_alpha1p0/'
 
 # Data assignment can be done through independent operations
 for snap_number in range(args.snapshot_number, 2523):
-    # if snap_number % num_processes == rank:
-    #     print(f"Rank {rank:03d} processing snapshot {snap_number:03d}")
+    if snap_number % num_processes == rank:
+        print(f"Rank {rank:03d} processing snapshot {snap_number:03d}")
 
-    s = dir + f"snapshots/{name}_{snap_number:04d}.hdf5"
-    c = dir + f"stf/{name}_{snap_number:04d}/{name}_{snap_number:04d}.properties"
-    gf = CoolingTimes()
+        s = dir + f"snapshots/{name}_{snap_number:04d}.hdf5"
+        c = dir + f"stf/{name}_{snap_number:04d}/{name}_{snap_number:04d}.properties"
+        gf = CoolingTimes()
 
-    try:
-        gf.process_single_halo(
-            path_to_snap=s,
-            path_to_catalogue=c,
-            agn_time=None,
-            z_agn_start=18,
-            z_agn_end=0
-        )
-    except Exception as e:
-        print(f"\n[!!!] Snap number {snap_number:04d} could not be processed.")
-        raise e
+        try:
+            gf.process_single_halo(
+                path_to_snap=s,
+                path_to_catalogue=c,
+                agn_time=None,
+                z_agn_start=18,
+                z_agn_end=0
+            )
+        except Exception as e:
+            print(f"\n[!!!] Snap number {snap_number:04d} could not be processed.")
+            raise e
+
+        del gf
