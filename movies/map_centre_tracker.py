cd202f6cbc9a0c20fa5db70121adff1f0f9a9fc5
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-05-17 20:26:36 +0100

    k

--- movies/map_centre_tracker.py
+++ movies/map_centre_tracker.py
@@ -27,10 +27,6 @@ def read(snapshot_number):
 
 dir = '/cosma/home/dp004/dc-alta2/snap7/xl-zooms/hydro/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_alpha1p0/'
 
-r500 = []
-xcminpot = []
-ycminpot = []
-zcminpot = []
 
 with Pool() as pool:
     results = list(tqdm(
