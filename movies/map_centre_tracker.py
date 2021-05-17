0414a08d1c00156fad7503735067384bd18340c1
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-05-17 20:19:44 +0100

    k

--- movies/map_centre_tracker.py
+++ movies/map_centre_tracker.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 
+def smooth(data, window_width):
+    cumsum_vec = np.cumsum(np.insert(data, 0, 0))
+    ma_vec = (cumsum_vec[window_width:] - cumsum_vec[:-window_width]) / window_width
+    return ma_vec
+
+
 dir = '/cosma/home/dp004/dc-alta2/snap7/xl-zooms/hydro/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_alpha1p0/'
 
 r500 = []
@@ -32,5 +38,18 @@ plt.plot(r500 - np.mean(r500), label='r500')
 plt.plot(xcminpot - np.mean(xcminpot), label='xcminpot')
 plt.plot(ycminpot - np.mean(ycminpot), label='ycminpot')
 plt.plot(zcminpot - np.mean(zcminpot), label='zcminpot')
+
+plt.plot(smooth(xcminpot - np.mean(xcminpot), 3), label='xcminpot_smooth3')
+plt.plot(smooth(ycminpot - np.mean(ycminpot), 3), label='ycminpot_smooth3')
+plt.plot(smooth(zcminpot - np.mean(zcminpot), 3), label='zcminpot_smooth3')
+
+plt.plot(smooth(xcminpot - np.mean(xcminpot), 7), label='xcminpot_smooth7')
+plt.plot(smooth(ycminpot - np.mean(ycminpot), 7), label='ycminpot_smooth7')
+plt.plot(smooth(zcminpot - np.mean(zcminpot), 7), label='zcminpot_smooth7')
+
+plt.plot(smooth(xcminpot - np.mean(xcminpot), 15), label='xcminpot_smooth15')
+plt.plot(smooth(ycminpot - np.mean(ycminpot), 15), label='ycminpot_smooth15')
+plt.plot(smooth(zcminpot - np.mean(zcminpot), 15), label='zcminpot_smooth15')
+
 plt.legend()
 plt.show()
