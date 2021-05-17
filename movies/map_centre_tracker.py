9da60cb7ed24d16b28d4ae6c46345d1479e004ae
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-05-17 21:11:11 +0100

    k

--- movies/map_centre_tracker.py
+++ movies/map_centre_tracker.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from multiprocessing import Pool
+from scipy.interpolate import interp1d
+from scipy import arange, array, exp
 
 
 def smooth(data, window_width):
@@ -16,6 +18,23 @@ def read(snapshot_number):
         f"stf/L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth_{snapshot_number:04d}/"
         f"L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.5_Nheat1_SNnobirth_{snapshot_number:04d}.properties"
     )
+
+def extrap1d(interpolator):
+    xs = interpolator.x
+    ys = interpolator.y
+
+    def pointwise(x):
+        if x < xs[0]:
+            return ys[0] + (x - xs[0]) * (ys[1] - ys[0]) / (xs[1] - xs[0])
+        elif x > xs[-1]:
+            return ys[-1] + (x - xs[-1]) * (ys[-1] - ys[-2]) / (xs[-1] - xs[-2])
+        else:
+            return interpolator(x)
+
+    def ufunclike(xs):
+        return array(list(map(pointwise, array(xs))))
+
+    return ufunclike
 
     vr_handle = velociraptor.load(path_to_catalogue)
     r500 = vr_handle.spherical_overdensities.r_500_rhocrit[0].to('Mpc').value
@@ -40,17 +59,26 @@ with Pool() as pool:
     zcminpot = np.asarray(list(results)).T[3]
 
 window = 150
-
+steps = np.arange(2523)
 r500_smoothed = smooth(r500 - np.mean(r500), window)
 xcminpot_smoothed = smooth(xcminpot, window)
 ycminpot_smoothed = smooth(ycminpot, window)
 zcminpot_smoothed = smooth(zcminpot, window)
 
+f_i = interp1d(steps[window // 2:-window // 2], xcminpot_smoothed)
+f_x = extrap1d(f_i)
+
+zcminpot_smoothed = np.r_[
+    f_x(steps[:window // 2]),
+    zcminpot_smoothed,
+    f_x(steps[-window // 2:]),
+]
+
 # plt.plot(r500[:l] - r500_smoothed, label='r500')
 # plt.plot(xcminpot[:l] - xcminpot_smoothed, label='xcminpot')
 # plt.plot(ycminpot[:l] - ycminpot_smoothed, label='ycminpot')
 plt.plot(zcminpot_smoothed, label='zcminpot_smoothed')
-plt.plot(zcminpot[window / 2:-window / 2], label='zcminpot')
+plt.plot(zcminpot, label='zcminpot')
 
 plt.legend()
 plt.show()
