b39b1021bc140b62ebdec335988bc22fb7c19c40
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-06-01 14:17:33 +0100

    k

--- testing/test_entropies.py
+++ testing/test_entropies.py
@@ -6,12 +6,12 @@ from scaling_relations import Entropies
 from test_files import cat, snap
 
 gf = Entropies()
-print(
-    gf.process_single_halo(
+single_halo_results = gf.process_single_halo(
         path_to_snap=snap,
         path_to_catalogue=cat
     )
-)
+for value in single_halo_results:
+    print(value)
 
 # print(
 #     gf.process_catalogue()
