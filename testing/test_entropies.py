2ff0b0bf1cecaf2da5d08eb241fd38eb6123a872
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-06-01 14:54:24 +0100

    k

--- testing/test_entropies.py
+++ testing/test_entropies.py
@@ -7,11 +7,14 @@ from test_files import cat, snap
 
 gf = Entropies()
 single_halo_results = gf.process_single_halo(
-        path_to_snap=snap,
-        path_to_catalogue=cat
-    )
-for value in single_halo_results:
-    print(value)
+    path_to_snap=snap,
+    path_to_catalogue=cat
+)
+for value, key in zip(
+        single_halo_results,
+        ['k30kpc', 'k0p15r500', 'k2500', 'k1500', 'k1000', 'k500', 'k200']
+):
+    print(key, value)
 
 # print(
 #     gf.process_catalogue()
