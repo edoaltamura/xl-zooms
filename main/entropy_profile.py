3207ce04beb2d4a547b253616de0b6c843e672c9
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-06-14 10:38:48 +0100

    Add Croston data

--- main/entropy_profile.py
+++ main/entropy_profile.py
@@ -32,9 +32,9 @@ set_mnras_stylesheet()
 fig = plt.figure(constrained_layout=True)
 axes = fig.add_subplot()
 
-make_profile(axes, xray_weighting=False, simple_electron_number_density=False, shell_average=True)
-make_profile(axes, xray_weighting=True, simple_electron_number_density=True, shell_average=False)
-make_profile(axes, xray_weighting=False, simple_electron_number_density=True, shell_average=True)
+make_profile(axes, xray_weighting=False, simple_electron_number_density=False, shell_average=False)
+# make_profile(axes, xray_weighting=True, simple_electron_number_density=True, shell_average=False)
+make_profile(axes, xray_weighting=False, simple_electron_number_density=True, shell_average=False)
 
 axes.set_xscale('log')
 axes.set_yscale('log')
