ec1261a0512da70e6c4e3422a2751abcda26a6ea
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-06-28 19:59:45 +0100

    Add Croston data

--- scaling_relations/entropy_plateau.py
+++ scaling_relations/entropy_plateau.py
@@ -38,7 +38,7 @@ class EntropyPlateau(HaloProperty):
 
     def __init__(
             self,
-            max_radius_r500: float = 4,
+            max_radius_r500: float = 6,
             simple_electron_number_density: bool = False,
     ):
         super().__init__()
