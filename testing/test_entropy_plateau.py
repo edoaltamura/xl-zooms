65d63438df1cd39c3965507f98c17bb78900e310
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-06-28 19:56:22 +0100

    Add Croston data

--- testing/test_entropy_plateau.py
+++ testing/test_entropy_plateau.py
@@ -23,6 +23,7 @@ plateau.setup_data(path_to_snap=snap, path_to_catalogue=cat)
 plateau.select_particles_on_plateau(shell_radius_r500=0.1, shell_thickness_r500=0.02, temperature_cut=True)
 particle_ids_z0p5 = plateau.get_particle_ids()
 print(f"Redshift {plateau.z:.3f}: {plateau.number_particles:d} particles selected")
+del plateau
 
 # Move to redshift 3 and track the same particle IDs
 plateau = EntropyPlateau()
