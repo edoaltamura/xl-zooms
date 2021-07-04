7d9a4aab0f2244451d925f7ca1b41fad4fda42ef
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-07-04 20:09:13 +0100

    Add roston data

--- main/sfr_central.py
+++ main/sfr_central.py
@@ -6,10 +6,10 @@ import unyt
 import sys
 sys.path.append("..")
 
-from register import xlargs, find_files
-
+from register import xlargs, find_files, set_mnras_stylesheet
+set_mnras_stylesheet()
 snap, cat = find_files()
-sfr_output_units = unyt.msun / (unyt.year * unyt.Mpc ** 3)
+sfr_output_units = unyt.msun / (unyt.year)# * unyt.Mpc ** 3)
 
 def set_snap_number(snap_number: int):
     old_snap_number = f"_{xlargs.snapshot_number:04d}"
@@ -25,16 +25,16 @@ sfr = np.empty(num_snaps)
 for i, snap_number in enumerate(snaps_collection[::-1]):
     vr_data = vrload(set_snap_number(snap_number)[1])
     print(i, snap_number, f"Redshift {vr_data.z:.3f}")
-    r200 = vr_data.radii.r_200crit[0]
-    volume = 4 / 3 * np.pi * r200 ** 3
+    # r200 = vr_data.radii.r_200crit[0]
+    # volume = 4 / 3 * np.pi * r200 ** 3
     redshifts[i] = vr_data.z
-    sfr[i] = (vr_data.star_formation_rate.sfr_gas[0] / volume).to(sfr_output_units)
+    sfr[i] = (vr_data.star_formation_rate.sfr_gas[0]).to(sfr_output_units)
 
 fig = plt.figure(constrained_layout=True)
 axes = fig.add_subplot()
 axes.set_yscale('log')
 axes.set_xlabel('Redshift')
-axes.set_ylabel(vr_data.star_formation_rate.sfr_gas.name)
+axes.set_ylabel(r"SFR $\dot{\rho}_*$ [M$_\odot$ yr$^{-1}$]")
 axes.plot(redshifts, sfr, color='g', linewidth=0.5, alpha=1)
 
 if not xlargs.quiet:
