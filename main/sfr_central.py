b88cdfc5117963bc892be11cb74f32a7f8738c46
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-07-04 15:50:00 +0100

    Add roston data

--- main/sfr_central.py
+++ main/sfr_central.py
@@ -6,14 +6,12 @@ from typing import Optional
 from matplotlib import pyplot as plt
 
 
-from unyt import (
-    unyt_array,
-    unyt_quantity,
-    mh, G, mp, K, kb, cm, Solar_Mass, Mpc, dimensionless
-)
+import unyt
 
 from register import xlargs, find_files
 snap, cat = find_files()
+
+sfr_output_units = unyt.msun / (unyt.year * unyt.Mpc ** 3)
 
 def set_snap_number(snap_number: int):
     old_snap_number = f"_{xlargs.snapshot_number:04d}"
@@ -29,8 +27,10 @@ sfr = np.empty(num_snaps)
 for i, snap_number in enumerate(snaps_collection[::-1]):
     vr_data = vrload(set_snap_number(snap_number)[1])
     print(i, snap_number, f"Redshift {vr_data.z:.3f}")
+    r200 = vr_data.radii.r_200crit[0]
+    volume = 4 / 3 * np.pi * r200 ** 3
     redshifts[i] = vr_data.z
-    sfr[i] = vr_data.star_formation_rate.sfr_gas[0]
+    sfr[i] = (vr_data.star_formation_rate.sfr_gas[0] / volume).to(sfr_output_units)
 
 fig = plt.figure(constrained_layout=True)
 axes = fig.add_subplot()
@@ -38,4 +38,11 @@ axes.set_yscale('log')
 axes.set_xlabel('Redshift')
 axes.set_ylabel(f"Temperature")
 axes.set_ylim(1e3, 1e10)
-axes.plot(temperatures[snii_flag_z0p5].T, color='g', linewidth=0.1, alpha=0.2)
+axes.plot(redshifts, sfr, color='g', linewidth=0.5, alpha=1)
+
+if not xlargs.quiet:
+    plt.show()
+
+plt.savefig('sfr_central.pdf')
+
+plt.close()
