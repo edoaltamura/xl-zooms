ac93f7df31912470314b4146e3596109a3ded3be
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-07-08 16:21:51 +0100

    Add roston data

--- main/sfr_central.py
+++ main/sfr_central.py
@@ -20,8 +20,8 @@ def set_snap_number(snap_number: int):
     return snap.replace(old_snap_number, new_snap_number), cat.replace(old_snap_number, new_snap_number)
 
 
-snaps_collection = np.arange(1, 2522, 20)
-# snaps_collection = np.arange(36)
+# snaps_collection = np.arange(1, 2522, 20)
+snaps_collection = np.arange(36)
 num_snaps = len(snaps_collection)
 redshifts = np.empty(num_snaps)
 sfr = np.empty(num_snaps)
