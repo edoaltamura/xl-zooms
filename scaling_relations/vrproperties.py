3e9ae642cca90856cdfb62d00344cbf607bb81fe
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-07-08 22:14:52 +0100

    Add roston data

--- scaling_relations/vrproperties.py
+++ scaling_relations/vrproperties.py
@@ -29,20 +29,37 @@ class VRProperties(HaloProperty):
     ):
         _, vr_data = self.get_handles_from_zoom(zoom_obj, path_to_snap, path_to_catalogue, **kwargs)
 
-        m500 = vr_data.spherical_overdensities.mass_500_rhocrit[0].to('Msun')
-        r500 = vr_data.spherical_overdensities.r_500_rhocrit[0].to('Mpc')
-        m2500 = vr_data.spherical_overdensities.mass_2500_rhocrit[0].to('Msun')
-        r2500 = vr_data.spherical_overdensities.r_2500_rhocrit[0].to('Mpc')
-        m1000 = vr_data.spherical_overdensities.mass_1000_rhocrit[0].to('Msun')
-        r1000 = vr_data.spherical_overdensities.r_1000_rhocrit[0].to('Mpc')
-        m200 = vr_data.masses.mass_200crit[0].to('Msun')
-        r200 = vr_data.radii.r_200crit[0].to('Mpc')
+        try:
+            m500 = vr_data.spherical_overdensities.mass_500_rhocrit[0].to('Msun')
+            r500 = vr_data.spherical_overdensities.r_500_rhocrit[0].to('Mpc')
+            m2500 = vr_data.spherical_overdensities.mass_2500_rhocrit[0].to('Msun')
+            r2500 = vr_data.spherical_overdensities.r_2500_rhocrit[0].to('Mpc')
+            m1000 = vr_data.spherical_overdensities.mass_1000_rhocrit[0].to('Msun')
+            r1000 = vr_data.spherical_overdensities.r_1000_rhocrit[0].to('Mpc')
+            m200 = vr_data.masses.mass_200crit[0].to('Msun')
+            r200 = vr_data.radii.r_200crit[0].to('Mpc')
 
-        m_star500c = vr_data.spherical_overdensities.mass_star_500_rhocrit[0].to('Msun')
-        m_star30kpc = vr_data.apertures.mass_star_50_kpc[0].to('Msun')
-        m_star100kpc = vr_data.apertures.mass_star_100_kpc[0].to('Msun')
-        sfr_100kpc = vr_data.apertures.sfr_gas_100_kpc[0].to('Msun/Gyr')
-        m_bh100kpc = vr_data.apertures.mass_bh_100_kpc[0].to('Msun')
+            m_star500c = vr_data.spherical_overdensities.mass_star_500_rhocrit[0].to('Msun')
+            m_star30kpc = vr_data.apertures.mass_star_50_kpc[0].to('Msun')
+            m_star100kpc = vr_data.apertures.mass_star_100_kpc[0].to('Msun')
+            sfr_100kpc = vr_data.apertures.sfr_gas_100_kpc[0].to('Msun/Gyr')
+            m_bh100kpc = vr_data.apertures.mass_bh_100_kpc[0].to('Msun')
+
+        except AttributeError:
+            m500 = None
+            r500 = None
+            m2500 = None
+            r2500 = None
+            m1000 = None
+            r1000 = None
+            m200 = None
+            r200 = None
+
+            m_star500c = None
+            m_star30kpc = None
+            m_star100kpc = None
+            sfr_100kpc = None
+            m_bh100kpc = None
 
         return (
             r2500, r1000, r500, r200, m2500, m1000, m500, m200, m_star500c, m_star30kpc, m_star100kpc,
