a2d8f9efe897a46d6627bb7fae278ff2624b49b6
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-07-19 14:18:55 +0200

    SN

--- main/entropy_profile_SNdT.py
+++ main/entropy_profile_SNdT.py
@@ -33,7 +33,7 @@ for sn_model in ['_SNdT7', '', '_SNdT8']:
     radial_bin_centres, entropy_profile, K500 = profile_obj.process_single_halo(
         path_to_snap=snap, path_to_catalogue=cat
     )
-    entropy_profile /= K500
+    # entropy_profile /= K500
 
     if not sn_model:
         sn_model = '_SNdT7.5'
@@ -50,60 +50,61 @@ axes.set_xscale('log')
 axes.set_yscale('log')
 
 axes.axvline(0.15, color='k', linestyle='--', lw=0.5, zorder=0)
-axes.set_ylabel(r'$K/K_{500}$')
+# axes.set_ylabel(r'$K/K_{500}$')
+axes.set_ylabel(r'$K$ [keV cm$^2$]')
 axes.set_xlabel(r'$r/r_{500}$')
 axes.set_ylim([5e-3, 20])
 axes.set_xlim([5e-3, 2.5])
 
-sun_observations = Sun2009()
-r_r500, S_S500_50, S_S500_10, S_S500_90 = sun_observations.get_shortcut()
-
-axes.errorbar(
-    r_r500,
-    S_S500_50,
-    yerr=(
-        S_S500_50 - S_S500_10,
-        S_S500_90 - S_S500_50
-    ),
-    fmt='o',
-    color='grey',
-    ecolor='lightgray',
-    elinewidth=0.5,
-    capsize=0,
-    markersize=2,
-    label=sun_observations.citation,
-    zorder=0
-)
-
-rexcess = Pratt2010(n_radial_bins=30)
-bin_median, bin_perc16, bin_perc84 = rexcess.combine_entropy_profiles(
-    m500_limits=(
-        1e14 * Solar_Mass,
-        5e14 * Solar_Mass
-    ),
-    k500_rescale=True
-)
-
-axes.errorbar(
-    rexcess.radial_bins,
-    bin_median,
-    yerr=(
-        bin_median - bin_perc16,
-        bin_perc84 - bin_median
-    ),
-    fmt='s',
-    color='grey',
-    ecolor='lightgray',
-    elinewidth=0.5,
-    capsize=0,
-    markersize=2,
-    label=rexcess.citation,
-    zorder=0
-)
-
-r = np.array([0.01, 1])
-k = 1.40 * r ** 1.1
-axes.plot(r, k, c='grey', ls='--', label='VKB (2005)', zorder=0)
+# sun_observations = Sun2009()
+# r_r500, S_S500_50, S_S500_10, S_S500_90 = sun_observations.get_shortcut()
+#
+# axes.errorbar(
+#     r_r500,
+#     S_S500_50,
+#     yerr=(
+#         S_S500_50 - S_S500_10,
+#         S_S500_90 - S_S500_50
+#     ),
+#     fmt='o',
+#     color='grey',
+#     ecolor='lightgray',
+#     elinewidth=0.5,
+#     capsize=0,
+#     markersize=2,
+#     label=sun_observations.citation,
+#     zorder=0
+# )
+#
+# rexcess = Pratt2010(n_radial_bins=30)
+# bin_median, bin_perc16, bin_perc84 = rexcess.combine_entropy_profiles(
+#     m500_limits=(
+#         1e14 * Solar_Mass,
+#         5e14 * Solar_Mass
+#     ),
+#     k500_rescale=True
+# )
+#
+# axes.errorbar(
+#     rexcess.radial_bins,
+#     bin_median,
+#     yerr=(
+#         bin_median - bin_perc16,
+#         bin_perc84 - bin_median
+#     ),
+#     fmt='s',
+#     color='grey',
+#     ecolor='lightgray',
+#     elinewidth=0.5,
+#     capsize=0,
+#     markersize=2,
+#     label=rexcess.citation,
+#     zorder=0
+# )
+#
+# r = np.array([0.01, 1])
+# k = 1.40 * r ** 1.1
+# axes.plot(r, k, c='grey', ls='--', label='VKB (2005)', zorder=0)
 
 axes.text(
     0.025,
