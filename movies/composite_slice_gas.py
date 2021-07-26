1c1830a1bae7cc86ea0dea4362f7d1f43a0cfd93
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-07-26 19:55:41 +0200

    Add extra profiles

--- movies/composite_slice_gas.py
+++ movies/composite_slice_gas.py
@@ -77,55 +77,55 @@ def draw_radius_contours(axes, resolution, region, r500, levels=[1.], color='gre
 
 
 def draw_panel(axes, field, cmap: str = 'Greys_r', vmin=None, vmax=None):
-    # gf = SliceGas(field, resolution=1024)
-    #
-    # try:
-    #     slice = gf.process_single_halo(
-    #         path_to_snap=s,
-    #         path_to_catalogue=c,
-    #         temperature_range=(1e5, 1e9),
-    #         depth_offset=None,  # Goes through the centre of potential
-    #         mask_radius_r500=4
-    #         # map_centre=centres[xlargs.snapshot_number, :-1]
-    #     )
-    #
-    # except Exception as e:
-    #     print(
-    #         f"Snap number {xlargs.snapshot_number:04d} could not be processed.",
-    #         e,
-    #         sep='\n'
-    #     )
-    #
-    # if xlargs.debug:
-    #     print(f"{field.title()} map: min = {np.nanmin(slice.map):.2E}, max = {np.nanmax(slice.map):.2E}")
-    #     print(f"Map centre: {[float(f'{i.v:.3f}') for i in slice.centre]} Mpc")
-    #
-    # cmap_bkgr = copy.copy(plt.get_cmap(cmap))
-    # cmap_bkgr.set_under('black')
-    # axes.axis("off")
-    # axes.set_aspect("equal")
-    # axes.imshow(
-    #     slice.map.T,
-    #     norm=LogNorm(vmin=vmin, vmax=vmax),
-    #     cmap=cmap_bkgr,
-    #     origin="lower",
-    #     extent=slice.region
-    # )
-    # axes.text(
-    #     0.025,
-    #     0.025,
-    #     (
-    #         f'{field.title()}\n'
-    #         f'z = {slice.z:.2f}\n'
-    #         f't = {Cosmology().age(slice.z).value:.3f} Gyr'
-    #     ),
-    #     color="w",
-    #     ha="left",
-    #     va="bottom",
-    #     alpha=0.8,
-    #     transform=axes.transAxes,
-    # )
-    draw_radius_contours(axes, resolution=1024, region=[-10, 10, -10, 10], r500=5, levels=[1.])
+    gf = SliceGas(field, resolution=1024)
+
+    try:
+        slice = gf.process_single_halo(
+            path_to_snap=s,
+            path_to_catalogue=c,
+            temperature_range=(1e5, 1e9),
+            depth_offset=None,  # Goes through the centre of potential
+            mask_radius_r500=4
+            # map_centre=centres[xlargs.snapshot_number, :-1]
+        )
+
+    except Exception as e:
+        print(
+            f"Snap number {xlargs.snapshot_number:04d} could not be processed.",
+            e,
+            sep='\n'
+        )
+
+    if xlargs.debug:
+        print(f"{field.title()} map: min = {np.nanmin(slice.map):.2E}, max = {np.nanmax(slice.map):.2E}")
+        print(f"Map centre: {[float(f'{i.v:.3f}') for i in slice.centre]} Mpc")
+
+    cmap_bkgr = copy.copy(plt.get_cmap(cmap))
+    cmap_bkgr.set_under('black')
+    axes.axis("off")
+    axes.set_aspect("equal")
+    axes.imshow(
+        slice.map.T,
+        norm=LogNorm(vmin=vmin, vmax=vmax),
+        cmap=cmap_bkgr,
+        origin="lower",
+        extent=slice.region
+    )
+    axes.text(
+        0.025,
+        0.025,
+        (
+            f'{field.title()}\n'
+            f'z = {slice.z:.2f}\n'
+            f't = {Cosmology().age(slice.z).value:.3f} Gyr'
+        ),
+        color="w",
+        ha="left",
+        va="bottom",
+        alpha=0.8,
+        transform=axes.transAxes,
+    )
+    draw_radius_contours(axes, resolution=len(slice.map), region=slice.region, r500=slice.r500, levels=[1.])
 
 
 fig = plt.figure(figsize=(9, 3), constrained_layout=True)
