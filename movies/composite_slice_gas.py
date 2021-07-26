84760e8c2b6490c9072a16187aade3aa45b2b610
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-07-26 20:55:13 +0200

    Add extra profiles

--- movies/composite_slice_gas.py
+++ movies/composite_slice_gas.py
@@ -33,18 +33,18 @@ class AnchoredHScaleBar(matplotlib.offsetbox.AnchoredOffsetbox):
 
     def __init__(self, size=1, extent=0.03, label="", loc=2, ax=None,
                  pad=0.4, borderpad=0.5, ppad=0, sep=2, prop=None,
-                 frameon=True, linekw={}, **kwargs):
+                 frameon=True, textkw={}, linekw={}, **kwargs):
         if not ax:
             ax = plt.gca()
         trans = ax.get_xaxis_transform()
         size_bar = matplotlib.offsetbox.AuxTransformBox(trans)
         line = Line2D([0, size], [0, 0], **linekw)
-        # vline1 = Line2D([0, 0], [-extent / 2., extent / 2.], **linekw)
-        # vline2 = Line2D([size, size], [-extent / 2., extent / 2.], **linekw)
+        vline1 = Line2D([0, 0], [-extent / 2., extent / 2.], **linekw)
+        vline2 = Line2D([size, size], [-extent / 2., extent / 2.], **linekw)
         size_bar.add_artist(line)
-        # size_bar.add_artist(vline1)
-        # size_bar.add_artist(vline2)
-        txt = matplotlib.offsetbox.TextArea(label, minimumdescent=False)
+        size_bar.add_artist(vline1)
+        size_bar.add_artist(vline2)
+        txt = matplotlib.offsetbox.TextArea(label, minimumdescent=False, textprops=textkw)
         self.vpac = matplotlib.offsetbox.VPacker(children=[size_bar, txt], align="center", pad=ppad, sep=sep)
         matplotlib.offsetbox.AnchoredOffsetbox.__init__(self, loc, pad=pad,
                                                         borderpad=borderpad, child=self.vpac, prop=prop,
@@ -160,7 +160,8 @@ def draw_panel(axes, field, cmap: str = 'Greys_r', vmin=None, vmax=None):
     draw_radius_contours(axes, slice, levels=[1.], color='w')
 
     ob = AnchoredHScaleBar(size=1, label="1 Mpc", loc=4, frameon=False,
-                           pad=0.6, sep=4, linekw=dict(color="white", linewidth=0.5))
+                           pad=0.6, sep=4, linekw=dict(color="white", linewidth=0.5),
+                           textkw=dict(color='white'))
     axes.add_artist(ob)
 
 
