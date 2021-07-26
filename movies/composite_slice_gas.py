0f71e6b3e7a1f1bd596cf9adaaefdc4079bf308e
edoaltamura <38359901+edoaltamura@users.noreply.github.com>
2021-07-26 19:32:50 +0200

    Add extra profiles

--- movies/composite_slice_gas.py
+++ movies/composite_slice_gas.py
@@ -24,9 +24,10 @@ centres = np.load('map_centre_L0300N0564_VR18_-8res_MinimumDistance_fixedAGNdT8.
 
 def draw_radius_contours(axes, slice, levels=[1.], color='green', r500_units=True, use_labels=True):
     # Make the norm object to define the image stretch
-    x_bins, y_bins = np.meshgrid(slice.map.T.shape[0], slice.map.T.shape[1])
+    x_bins, y_bins = np.meshgrid(*slice.map.shape)
     cylinder_function = np.sqrt(x_bins.flatten() ** 2 + y_bins.flatten() ** 2)
-    cylinder_function = cylinder_function.reshape(x_bins.shape)
+    cylinder_function = cylinder_function.reshape(slice.map.shape)
+    print(cylinder_function)
     _levels = [radius * slice.r500 for radius in levels] if r500_units else levels
     _units = r'$r_{500}$' if r500_units else 'Mpc'
 
